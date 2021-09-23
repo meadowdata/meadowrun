meadowdata/meadowrun

commit 3db366a6b38da1b876b3d0ab4844c1acb1734b18

Purely a formatting change. Combining jobs and jobs_common. Dealing with previously circular imports: - with topic by using import nextbeat.topic instead of from nextbeat.topic from - with local_job_runner/nextrun_job_runner by moving JobRunnerTypePredicate to a separate (smaller) file. This allows us to "unquote" the type hint in Effects.add_jobs

src/nextbeat/scheduler.py
@@ -5,8 +5,7 @@ from asyncio import Task
 from typing import Dict, List, Tuple, Iterable, Optional, Callable, Awaitable
 
 from nextbeat.event_log import Event, EventLog, Timestamp
-from nextbeat.jobs import Actions, Job
-from nextbeat.jobs_common import JobPayload, JobRunner
+from nextbeat.jobs import Actions, Job, JobPayload, JobRunner
 from nextbeat.local_job_runner import LocalJobRunner
 from nextbeat.time_event_publisher import TimeEventPublisher
 from nextbeat.topic import Action, Topic, Trigger

src/nextbeat/effects.py
@@ -3,8 +3,7 @@ These functions should be called from inside of nextbeat jobs to indicate that a
 has happened in that job.
 """
 
-from nextbeat.jobs import Job
-from nextbeat.jobs_common import Effects
+from nextbeat.jobs import Job, Effects
 
 # effects are processed in Scheduler._process_effects
 _effects = Effects([])

src/nextbeat/job_runner_predicates.py
@@ -1,0 +1,55 @@
+"""
+The base JobRunnerPredicate class is in jobs.py, but the implementations are here
+partially for organization and partially because it's too hard to deal with the circular
+imports.
+"""
+
+from typing import Dict, Type, Any
+
+import nextbeat.local_job_runner
+import nextbeat.nextrun_job_runner
+from nextbeat.jobs import JobRunnerPredicate, JobRunner
+
+
+class AndPredicate(JobRunnerPredicate):
+    def __init__(self, a: JobRunnerPredicate, b: JobRunnerPredicate):
+        self._a = a
+        self._b = b
+
+    def apply(self, job_runner: JobRunner) -> bool:
+        return self._a.apply(job_runner) and self._b.apply(job_runner)
+
+
+class OrPredicate(JobRunnerPredicate):
+    def __init__(self, a: JobRunnerPredicate, b: JobRunnerPredicate):
+        self._a = a
+        self._b = b
+
+    def apply(self, job_runner: JobRunner) -> bool:
+        return self._a.apply(job_runner) or self._b.apply(job_runner)
+
+
+class ValueInPropertyPredicate(JobRunnerPredicate):
+    """E.g. ValueInPropertyPredicate("capabilities", "chrome")"""
+
+    def __init__(self, property_name: str, value: Any):
+        self._property_name = property_name
+        self._value = value
+
+    def apply(self, job_runner: JobRunner) -> bool:
+        return self._value in getattr(job_runner, self._property_name)
+
+
+_JOB_RUNNER_TYPES: Dict[str, Type] = {
+    "local": nextbeat.local_job_runner.LocalJobRunner,
+    "nextrun": nextbeat.nextrun_job_runner.NextRunJobRunner,
+}
+
+
+class JobRunnerTypePredicate(JobRunnerPredicate):
+    def __init__(self, job_runner_type: str):
+        self._job_runner_type = _JOB_RUNNER_TYPES[job_runner_type]
+
+    def apply(self, job_runner: JobRunner) -> bool:
+        # noinspection PyTypeHints
+        return isinstance(job_runner, self._job_runner_type)

src/nextbeat/jobs.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 import abc
+import dataclasses
 import random
 import uuid
 from dataclasses import dataclass
@@ -10,23 +13,119 @@ from typing import (
     Sequence,
     List,
     Any,
-    Type,
     Dict,
     Optional,
     Union,
+    Literal,
+    Callable,
 )
 
 from nextbeat.event_log import EventLog, Event, Timestamp
-from nextbeat.jobs_common import (
-    JobRunner,
-    JobState,
-    JobRunnerFunction,
-    JobRunnerFunctionTypes,
-    VersionedJobRunnerFunction,
-)
-from nextbeat.local_job_runner import LocalJobRunner
-from nextbeat.nextrun_job_runner import NextRunJobRunner
-from nextbeat.topic import Topic, Action, Trigger
+import nextbeat.topic
+from nextrun.deployed_function import NextRunDeployedFunction
+
+
+JobState = Literal[
+    # Nothing is currently happening with the job
+    "WAITING",
+    # A run of the job has been requested on a job runner
+    "RUN_REQUESTED",
+    # The job is currently running. JobPayload.pid will be populated
+    "RUNNING",
+    # The job has completed normally. JobPayload.result_value and pid will be populated
+    "SUCCEEDED",
+    # The job was cancelled by the user. JobPayload.pid will be populated
+    "CANCELLED",
+    # The job failed. JobPayload.failure_type and pid will be populated. If failure_type
+    # is PYTHON_EXCEPTION or RUN_REQUEST_FAILED, raised_exception will be populated, if
+    # failure_type is NON_ZERO_RETURN_CODE, return_code will be populated
+    "FAILED",
+]
+
+
+@dataclass(frozen=True)
+class RaisedException:
+    """Represents a python exception raised by a remote process"""
+
+    exception_type: str
+    exception_message: str
+    exception_traceback: str
+
+
+@dataclasses.dataclass(frozen=True)
+class Effects:
+    """Represents effects created by the running of a job"""
+
+    add_jobs: List[Job]
+
+
+@dataclass(frozen=True)
+class JobPayload:
+    """The Event.payload for Job-related events. See JobStateType docstring."""
+
+    request_id: Optional[str]
+    state: JobState
+    failure_type: Optional[
+        Literal["PYTHON_EXCEPTION", "NON_ZERO_RETURN_CODE", "RUN_REQUEST_FAILED"]
+    ] = None
+    pid: Optional[int] = None
+    result_value: Any = None
+    effects: Optional[Effects] = None
+    raised_exception: Union[RaisedException, BaseException, None] = None
+    return_code: Optional[int] = None
+
+
+@dataclasses.dataclass(frozen=True)
+class LocalFunction:
+    """
+    A function pointer in the current codebase with arguments for calling the
+    function
+    """
+
+    function_pointer: Callable[..., Any]
+    function_args: Sequence[Any] = dataclasses.field(default_factory=lambda: [])
+    function_kwargs: Dict[str, Any] = dataclasses.field(default_factory=lambda: {})
+
+
+JobRunnerFunctionTypes = (LocalFunction, NextRunDeployedFunction)
+# A JobRunnerFunction is a function/executable/script that one or more JobRunners will
+# know how to run along with the arguments for that function/executable/script
+JobRunnerFunction = Union[JobRunnerFunctionTypes]
+
+
+class JobRunner(abc.ABC):
+    """An interface for job runner clients"""
+
+    @abc.abstractmethod
+    async def run(
+        self, job_name: str, run_request_id: str, job_runner_function: JobRunnerFunction
+    ) -> None:
+        pass
+
+    @abc.abstractmethod
+    async def poll_jobs(self, last_events: Iterable[Event[JobPayload]]) -> None:
+        """
+        last_events is the last event we've recorded for the jobs that we are interested
+        in. poll_jobs will add new events to the EventLog for these jobs if there's been
+        any change in their state.
+        """
+        pass
+
+
+class VersionedJobRunnerFunction(abc.ABC):
+    """
+    Similar to a JobRunnerFunction, but instead of a single version of the code (e.g. a
+    specific commit in a git repo), specifies a versioned codebase (e.g. a git repo),
+    along with a function/executable/script in that repo (and also including the
+    arguments to call that function/executable/script).
+
+    TODO this is not yet fully fleshed out and the interface will probably need to
+     change
+    """
+
+    @abc.abstractmethod
+    def get_job_runner_function(self) -> JobRunnerFunction:
+        pass
 
 
 class JobRunnerPredicate(abc.ABC):
@@ -37,55 +136,11 @@ class JobRunnerPredicate(abc.ABC):
         pass
 
 
-class AndPredicate(JobRunnerPredicate):
-    def __init__(self, a: JobRunnerPredicate, b: JobRunnerPredicate):
-        self._a = a
-        self._b = b
-
-    def apply(self, job_runner: JobRunner) -> bool:
-        return self._a.apply(job_runner) and self._b.apply(job_runner)
-
-
-class OrPredicate(JobRunnerPredicate):
-    def __init__(self, a: JobRunnerPredicate, b: JobRunnerPredicate):
-        self._a = a
-        self._b = b
-
-    def apply(self, job_runner: JobRunner) -> bool:
-        return self._a.apply(job_runner) or self._b.apply(job_runner)
-
-
-class ValueInPropertyPredicate(JobRunnerPredicate):
-    """E.g. ValueInPropertyPredicate("capabilities", "chrome")"""
-
-    def __init__(self, property_name: str, value: Any):
-        self._property_name = property_name
-        self._value = value
-
-    def apply(self, job_runner: JobRunner) -> bool:
-        return self._value in getattr(job_runner, self._property_name)
-
-
-_JOB_RUNNER_TYPES: Dict[str, Type] = {
-    "local": LocalJobRunner,
-    "nextrun": NextRunJobRunner,
-}
-
-
-class JobRunnerTypePredicate(JobRunnerPredicate):
-    def __init__(self, job_runner_type: str):
-        self._job_runner_type = _JOB_RUNNER_TYPES[job_runner_type]
-
-    def apply(self, job_runner: JobRunner) -> bool:
-        # noinspection PyTypeHints
-        return isinstance(job_runner, self._job_runner_type)
-
-
 JobFunction = Union[JobRunnerFunction, VersionedJobRunnerFunction]
 
 
 @dataclass(frozen=True)
-class Job(Topic):
+class Job(nextbeat.topic.Topic):
     """
     A job runs python code (specified job_run_spec) on a job_runner. The scheduler will
     also perform actions automatically based on trigger_actions.
@@ -102,14 +157,14 @@ class Job(Topic):
     job_function: JobFunction
 
     # explains what actions to take when
-    trigger_actions: Tuple[Tuple[Trigger, Action], ...]
+    trigger_actions: Tuple[Tuple[nextbeat.topic.Trigger, nextbeat.topic.Action], ...]
 
     # specifies which job runners this job can run on
     job_runner_predicate: Optional[JobRunnerPredicate] = None
 
 
 @dataclass(frozen=True)
-class Run(Action):
+class Run(nextbeat.topic.Action):
     """Runs the job"""
 
     async def execute(
@@ -173,7 +228,7 @@ class Actions:
 
 
 @dataclass(frozen=True)
-class JobStateChangeTrigger(Trigger):
+class JobStateChangeTrigger(nextbeat.topic.Trigger):
     """A trigger for when a job changes JobState"""
 
     job_name: str

src/nextbeat/jobs_common.py
@@ -1,122 +1,0 @@
-"""
-This all thematically belongs in jobs.py but we broke it out to avoid circular imports
-"""
-
-import abc
-import dataclasses
-from dataclasses import dataclass
-from typing import (
-    Any,
-    Optional,
-    Iterable,
-    Union,
-    Literal,
-    Callable,
-    Sequence,
-    Dict,
-    List,
-)
-
-from nextbeat.event_log import Event
-from nextrun.deployed_function import NextRunDeployedFunction
-
-JobState = Literal[
-    # Nothing is currently happening with the job
-    "WAITING",
-    # A run of the job has been requested on a job runner
-    "RUN_REQUESTED",
-    # The job is currently running. JobPayload.pid will be populated
-    "RUNNING",
-    # The job has completed normally. JobPayload.result_value and pid will be populated
-    "SUCCEEDED",
-    # The job was cancelled by the user. JobPayload.pid will be populated
-    "CANCELLED",
-    # The job failed. JobPayload.failure_type and pid will be populated. If failure_type
-    # is PYTHON_EXCEPTION or RUN_REQUEST_FAILED, raised_exception will be populated, if
-    # failure_type is NON_ZERO_RETURN_CODE, return_code will be populated
-    "FAILED",
-]
-
-
-@dataclass(frozen=True)
-class RaisedException:
-    """Represents a python exception raised by a remote process"""
-
-    exception_type: str
-    exception_message: str
-    exception_traceback: str
-
-
-@dataclasses.dataclass(frozen=True)
-class Effects:
-    """Represents effects created by the running of a job"""
-    add_jobs: List["Job"]
-
-
-@dataclass(frozen=True)
-class JobPayload:
-    """The Event.payload for Job-related events. See JobStateType docstring."""
-
-    request_id: Optional[str]
-    state: JobState
-    failure_type: Optional[
-        Literal["PYTHON_EXCEPTION", "NON_ZERO_RETURN_CODE", "RUN_REQUEST_FAILED"]
-    ] = None
-    pid: Optional[int] = None
-    result_value: Any = None
-    effects: Optional[Effects] = None
-    raised_exception: Union[RaisedException, BaseException, None] = None
-    return_code: Optional[int] = None
-
-
-@dataclasses.dataclass(frozen=True)
-class LocalFunction:
-    """
-    A function pointer in the current codebase with arguments for calling the
-    function
-    """
-
-    function_pointer: Callable[..., Any]
-    function_args: Sequence[Any] = dataclasses.field(default_factory=lambda: [])
-    function_kwargs: Dict[str, Any] = dataclasses.field(default_factory=lambda: {})
-
-
-# A JobRunnerFunction is a function/executable/script that one or more JobRunners will
-# know how to run along with the arguments for that function/executable/script
-JobRunnerFunctionTypes = (LocalFunction, NextRunDeployedFunction)
-JobRunnerFunction = Union[JobRunnerFunctionTypes]
-
-
-class JobRunner(abc.ABC):
-    """An interface for job runner clients"""
-
-    @abc.abstractmethod
-    async def run(
-        self, job_name: str, run_request_id: str, job_runner_function: JobRunnerFunction
-    ) -> None:
-        pass
-
-    @abc.abstractmethod
-    async def poll_jobs(self, last_events: Iterable[Event[JobPayload]]) -> None:
-        """
-        last_events is the last event we've recorded for the jobs that we are interested
-        in. poll_jobs will add new events to the EventLog for these jobs if there's been
-        any change in their state.
-        """
-        pass
-
-
-class VersionedJobRunnerFunction(abc.ABC):
-    """
-    Similar to a JobRunnerFunction, but instead of a single version of the code (e.g. a
-    specific commit in a git repo), specifies a versioned codebase (e.g. a git repo),
-    along with a function/executable/script in that repo (and also including the
-    arguments to call that function/executable/script).
-
-    TODO this is not yet fully fleshed out and the interface will probably need to
-     change
-    """
-
-    @abc.abstractmethod
-    def get_job_runner_function(self) -> JobRunnerFunction:
-        pass

src/nextbeat/local_job_runner.py
@@ -7,7 +7,7 @@ from typing import Dict, Iterable
 from concurrent.futures import ProcessPoolExecutor, Future, CancelledError
 
 from nextbeat.event_log import Event, EventLog
-from nextbeat.jobs_common import JobPayload, JobRunner, LocalFunction, JobRunnerFunction
+from nextbeat.jobs import JobPayload, JobRunner, LocalFunction, JobRunnerFunction
 
 
 class LocalJobRunner(JobRunner):

src/nextbeat/nextrun_job_runner.py
@@ -3,12 +3,12 @@ import dataclasses
 from typing import Iterable
 
 from nextbeat.event_log import Event, EventLog
-from nextbeat.jobs_common import (
-    JobPayload,
-    JobRunner,
+from nextbeat.jobs import (
     RaisedException,
+    JobPayload,
     LocalFunction,
     JobRunnerFunction,
+    JobRunner,
     VersionedJobRunnerFunction,
 )
 from nextrun.client import NextRunClientAsync, ProcessStateEnum

src/nextbeat/topic.py
@@ -8,7 +8,7 @@ import itertools
 from typing import Iterable, Mapping, Sequence, List
 
 from nextbeat.event_log import EventLog, Event, Timestamp
-from nextbeat.jobs_common import JobRunner
+import nextbeat.jobs
 
 
 @dataclass(frozen=True)
@@ -40,7 +40,7 @@ class Action(ABC):
     async def execute(
         self,
         topic: Topic,
-        available_job_runners: List[JobRunner],
+        available_job_runners: List[nextbeat.jobs.JobRunner],
         event_log: EventLog,
         timestamp: Timestamp,
     ) -> None:

commit afd40f0f12a4410707f6af34ab54f2714a67d559

Minor changes: - Adding a few TODOs for bugs to fix/improvements to make - Changing the timing on test_time_event_publisher_point_in_time to make it less flaky - Reordering code in time_event_publisher to make it more consistent - Dealing with a weird circular import error introduced in 3db366a

src/nextbeat/event_log.py
@@ -186,6 +186,10 @@ class EventLog:
                 subscribers.update(self._topic_subscribers.get(event.topic_name, set()))
 
             # call the subscribers
+            # TODO I think ideal behavior here would be to retry failures 3 times or
+            #  something before giving up on a subscriber. Current behavior is very
+            #  bad--failure in one subscriber means we will never progress. I think the
+            #  outer try/except is probably not necessary.
             await asyncio.gather(
                 *(
                     subscriber(low_timestamp, high_timestamp)

src/nextbeat/scheduler.py
@@ -140,6 +140,9 @@ class Scheduler:
                             job, self._job_runners, self._event_log, high_timestamp
                         )
 
+                # TODO we should consider throwing an exception if the topic does not
+                #  already exist (otherwise there's actually no point in breaking out
+                #  this create_job_subscriptions into a separate function)
                 self._event_log.subscribe(
                     trigger.topic_names_to_subscribe(), subscriber
                 )

src/nextbeat/time_event_publisher.py
@@ -51,17 +51,16 @@ def _timedelta_to_str(t: datetime.timedelta) -> str:
 
 
 @dataclass(frozen=True)
-class TimeOfDayPayload:
-    # The original local_time_of_day parameter used to create the trigger
-    local_time_of_day: datetime.timedelta
-    # The original time zone used to create the trigger
-    time_zone: PytzTzInfo
-    # The date we're triggering for. Note that this does not have to be the current day
-    # (e.g. if local_time_of_day is <0 or >24hours)
-    date: datetime.date
-    # The point_in_time we're triggering for which should be roughly now, localized to
-    # time_zone
-    point_in_time: datetime.datetime
+class PointInTimeTrigger(Trigger):
+    """See TimeEventPublisher.point_in_time_trigger"""
+
+    topic_name: str
+
+    def topic_names_to_subscribe(self) -> Iterable[str]:
+        yield self.topic_name
+
+    def is_active(self, events: Mapping[str, Sequence[Event]]) -> bool:
+        return len(events[self.topic_name]) > 0
 
 
 @dataclass(frozen=True)
@@ -81,16 +80,17 @@ class TimeOfDayTrigger(Trigger):
 
 
 @dataclass(frozen=True)
-class PointInTimeTrigger(Trigger):
-    """See TimeEventPublisher.point_in_time_trigger"""
-
-    topic_name: str
-
-    def topic_names_to_subscribe(self) -> Iterable[str]:
-        yield self.topic_name
-
-    def is_active(self, events: Mapping[str, Sequence[Event]]) -> bool:
-        return len(events[self.topic_name]) > 0
+class TimeOfDayPayload:
+    # The original local_time_of_day parameter used to create the trigger
+    local_time_of_day: datetime.timedelta
+    # The original time zone used to create the trigger
+    time_zone: PytzTzInfo
+    # The date we're triggering for. Note that this does not have to be the current day
+    # (e.g. if local_time_of_day is <0 or >24hours)
+    date: datetime.date
+    # The point_in_time we're triggering for which should be roughly now, localized to
+    # time_zone
+    point_in_time: datetime.datetime
 
 
 class TimeEventPublisher:
@@ -101,6 +101,8 @@ class TimeEventPublisher:
     For any two time events t0 < t1, we can only guarantee that t0 will hit the event
     log before t1 if t0 is added before t1. Practically speaking, even if t1 is added
     before t0, if t0 is added sufficiently in advance, ordering will be as expected.
+
+    TODO what if time goes backwards (due to NTP or manually setting clocks?)
     """
 
     def __init__(
@@ -160,6 +162,9 @@ class TimeEventPublisher:
             trigger = self._all_point_in_time_events[name]
         else:
             trigger = PointInTimeTrigger("time/" + name)
+            # TODO it isn't really right to schedule this trigger here--it should really
+            #  get created when a job containing this trigger gets added to the
+            #  Scheduler.
             self._call_at.call_at(dt, lambda: self._append_event("time/" + name, dt))
             self._all_point_in_time_events[name] = trigger
 
@@ -231,6 +236,9 @@ class TimeEventPublisher:
                 self._all_time_of_day_triggers[name] = trigger
                 if self._schedule_recurring_up_to is not None:
                     now = _utc_now()
+                    # TODO it isn't really right to schedule this trigger here--it
+                    #  should really get created when a job containing this trigger gets
+                    #  added to the Scheduler.
                     self._schedule_time_of_day_trigger(
                         trigger, now, self._schedule_recurring_up_to
                     )

tests/test_nextbeat/test_time_events.py
@@ -7,6 +7,10 @@ import threading
 import time
 import asyncio
 
+# not sure what's going on here, but importing this prevents the next import from
+# causing a circular import error (but this only happens when we run pytest on a single
+# test in this module).
+import nextbeat.jobs
 from nextbeat.time_event_publisher import (
     TimeEventPublisher,
     TimeOfDayPayload,
@@ -119,9 +123,9 @@ def test_time_event_publisher_point_in_time():
 
         dts = [
             now.astimezone(tz_ny) - _TIME_INCREMENT,
-            now.astimezone(tz_la) + _TIME_INCREMENT,
-            now.astimezone(tz_ldn) + _TIME_INCREMENT,
+            now.astimezone(tz_la) + 2 * _TIME_INCREMENT,
             now.astimezone(tz_ldn) + 2 * _TIME_INCREMENT,
+            now.astimezone(tz_ldn) + 3 * _TIME_INCREMENT,
         ]
 
         for dt in dts:
@@ -137,7 +141,7 @@ def test_time_event_publisher_point_in_time():
         assert 1 == len(event_log._event_log)
         assert dt_strings[0] == _dt_to_str(event_log._event_log[0].payload)
 
-        time.sleep(_TIME_INCREMENT.total_seconds())
+        time.sleep(2 * _TIME_INCREMENT.total_seconds())
 
         assert 3 == len(event_log._event_log)
         # make sure that 2 times with the same point in time but different timezones
028487fa82983b39e4ae1dff40f50337f54049e9
Hyunho Richard Lee <hrichardlee@gmail.com>
2022-02-04 18:45:58 -0500

    Extending runner.py to also work with commands

--- src/meadowgrid/runner.py
+++ src/meadowgrid/runner.py
@@ -1,5 +1,8 @@
 import io
+import os.path
 import pickle
+import shlex
+import uuid
 from typing import Callable, TypeVar, Union, Any, Dict, Optional, Sequence
 
 import fabric
@@ -21,7 +24,7 @@ from meadowgrid.deployed_function import (
     VersionedInterpreterDeployment,
 )
 from meadowgrid.grid import _get_id_name_function
-from meadowgrid.meadowgrid_pb2 import JobToRun, Job, ProcessState
+from meadowgrid.meadowgrid_pb2 import JobToRun, Job, ProcessState, PyCommandJob
 
 
 _T = TypeVar("_T")
@@ -64,6 +67,37 @@ def _construct_job_to_run_function(
     return JobToRun(job=job)
 
 
+def _construct_job_to_run_command(
+    args: Union[str, Sequence[str]],
+    interpreter_deployment: Union[
+        InterpreterDeployment, VersionedInterpreterDeployment
+    ],
+    code_deployment: Union[CodeDeployment, VersionedCodeDeployment, None] = None,
+    environment_variables: Optional[Dict[str, str]] = None,
+) -> JobToRun:
+    """Basically a "user-friendly" JobToRun constructor for commands."""
+
+    job_id = str(uuid.uuid4())
+    if isinstance(args, str):
+        args = shlex.split(args)
+    # this is kind of a silly way to get a friendly name--treat the first three
+    # elements of args as if they're paths and take the last part of each path
+    friendly_name = "-".join(os.path.basename(arg) for arg in args[:3])
+
+    if code_deployment is None:
+        code_deployment = ServerAvailableFolder()
+
+    job = Job(
+        job_id=_make_valid_job_id(job_id),
+        job_friendly_name=_make_valid_job_id(friendly_name),
+        environment_variables=_string_pairs_from_dict(environment_variables),
+        result_highest_pickle_protocol=pickle.HIGHEST_PROTOCOL,
+        py_command=PyCommandJob(command_line=args),
+    )
+    _add_deployments_to_job(job, code_deployment, interpreter_deployment)
+    return JobToRun(job=job)
+
+
 async def run_function_async(
     function: Callable[..., _T],
     interpreter_deployment: Union[
@@ -97,6 +131,9 @@ async def run_function_async(
         raise ValueError(f"Error: {result.state}")
 
 
+# We should have run_command_async for completeness
+
+
 def run_function_remote(
     function: Callable[..., _T],
     host: str,
@@ -126,7 +163,36 @@ def run_function_remote(
         kwargs,
         environment_variables,
     )
+    return _run_remote(job_to_run, host, fabric_kwargs)
 
+
+def run_command_remote(
+    args: Union[str, Sequence[str]],
+    host: str,
+    interpreter_deployment: Union[
+        InterpreterDeployment, VersionedInterpreterDeployment
+    ],
+    code_deployment: Union[CodeDeployment, VersionedCodeDeployment, None] = None,
+    environment_variables: Optional[Dict[str, str]] = None,
+    # these options are forwarded directly to Fabric
+    fabric_kwargs: Optional[Dict[str, Any]] = None,
+) -> None:
+    """
+    Runs the specified command on a remote machine. See run_function_remote for more
+    details on requirements for the remote host.
+    """
+    job_to_run = _construct_job_to_run_command(
+        args, interpreter_deployment, code_deployment, environment_variables
+    )
+    _run_remote(job_to_run, host, fabric_kwargs)
+
+
+def _run_remote(
+    job_to_run: JobToRun,
+    host: str,
+    # these options are forwarded directly to Fabric
+    fabric_kwargs: Optional[Dict[str, Any]] = None,
+) -> Any:
     if fabric_kwargs is None:
         fabric_kwargs = {}
 
@@ -163,24 +229,29 @@ def run_function_remote(
         if result.return_code != 0:
             raise ValueError(f"Process exited {result.return_code}")
 
-        # get the state
-        with io.BytesIO() as state_buffer:
-            connection.get(f"{job_io_prefix}.state", state_buffer)
-            state_buffer.seek(0)
-            with io.TextIOWrapper(state_buffer, encoding="utf-8") as wrapper:
-                state_string = wrapper.read()
+        job_spec_type = job_to_run.job.WhichOneof("job_spec")
 
-        # Next get the result. The result file is optional for py_commands because we
-        # don't have full control over the process and there's no way to guarantee that
-        # "our code" gets executed
-        with io.BytesIO() as result_buffer:
-            connection.get(f"{job_io_prefix}.result", result_buffer)
-            result_buffer.seek(0)
-            result = pickle.load(result_buffer)
+        if job_spec_type == "py_command":
+            return None
+        elif job_spec_type == "py_function":
+            # get the state
+            with io.BytesIO() as state_buffer:
+                connection.get(f"{job_io_prefix}.state", state_buffer)
+                state_buffer.seek(0)
+                with io.TextIOWrapper(state_buffer, encoding="utf-8") as wrapper:
+                    state_string = wrapper.read()
 
-        if state_string == "SUCCEEDED":
-            return result[0]  # drop effects
-        elif state_string == "PYTHON_EXCEPTION":
-            raise ValueError("Python exception in remote process") from result
+            # next get the result
+            with io.BytesIO() as result_buffer:
+                connection.get(f"{job_io_prefix}.result", result_buffer)
+                result_buffer.seek(0)
+                result = pickle.load(result_buffer)
+
+            if state_string == "SUCCEEDED":
+                return result[0]  # drop effects
+            elif state_string == "PYTHON_EXCEPTION":
+                raise ValueError("Python exception in remote process") from result
+            else:
+                raise ValueError(f"Unknown state string: {state_string}")
         else:
-            raise ValueError(f"Unknown state string: {state_string}")
+            raise ValueError(f"Unknown job_spec {job_spec_type}")
--- tests/test_meadowgrid/test_runner.py
+++ tests/test_meadowgrid/test_runner.py
@@ -2,7 +2,11 @@ from typing import Any, Dict
 
 from meadowgrid.config import MEADOWGRID_INTERPRETER
 from meadowgrid.meadowgrid_pb2 import ServerAvailableInterpreter
-from meadowgrid.runner import run_function_async, run_function_remote
+from meadowgrid.runner import (
+    run_command_remote,
+    run_function_async,
+    run_function_remote,
+)
 
 
 async def test_run_function_local():
@@ -14,21 +18,34 @@ async def test_run_function_local():
     assert result == 10
 
 
-def manual_test_run_function_remote():
-    # these parameters must be changed!
+# these parameters must be changed!
 
-    # this must be a linux host with meadowgrid installed as per
-    # build_meadowgrid_amis.sh
-    host = "localhost"
-    # whatever parameters are needed to connect to the host via fabric
-    fabric_kwargs: Dict[str, Any] = {}
+# this must be a linux host with meadowgrid installed as per
+# build_meadowgrid_amis.sh
+_HOST = "localhost"
+# whatever parameters are needed to connect to the host via fabric
+_FABRIC_KWARGS: Dict[str, Any] = {}
+
+
+def manual_test_run_function_remote():
 
     result = run_function_remote(
         lambda x: x * 2,
-        host,
+        _HOST,
         ServerAvailableInterpreter(interpreter_path=MEADOWGRID_INTERPRETER),
         args=[5],
-        fabric_kwargs=fabric_kwargs,
+        fabric_kwargs=_FABRIC_KWARGS,
     )
 
     assert result == 10
+
+
+def manual_test_run_command_remote():
+    run_command_remote(
+        "pip --version",
+        _HOST,
+        ServerAvailableInterpreter(interpreter_path=MEADOWGRID_INTERPRETER),
+        fabric_kwargs=_FABRIC_KWARGS,
+    )
+    # right now we don't get the stdout back, so the only way to check this is to look
+    # at the log file on the remote host

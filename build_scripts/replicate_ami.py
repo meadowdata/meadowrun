9e1277f65aedb38d4cafac37523ea43c5e71da1f
Hyunho Richard Lee <hrichardlee@gmail.com>
2022-03-30 11:01:31 -0400

    Publishing a new pip package

--- build_scripts/replicate_ami.py
+++ build_scripts/replicate_ami.py
@@ -7,7 +7,7 @@ from meadowrun.aws_integration.management_lambdas.ec2_alloc_stub import (
 )
 
 _SOURCE_REGION = "us-east-2"  # the region that you ran build_ami.py under
-_SOURCE_AMI = "ami-04df04d188d242faf"  # the original AMI id in that region
+_SOURCE_AMI = "ami-074049d9b7ca1605a"  # the original AMI id in that region
 _SUPPORTED_REGIONS = [
     "us-east-1",
     "us-east-2",

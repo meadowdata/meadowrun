37cb9d0d3494cbe9f1195b252d45a7cf65e08fcf
Hyunho Richard Lee <hrichardlee@gmail.com>
2022-04-07 14:46:04 -0400

    Adding a missing check to only set the WindowsProactorEventLoopPolicy when we're actually running on Windows

--- src/meadowrun/__init__.py
+++ src/meadowrun/__init__.py
@@ -1,11 +1,12 @@
-import asyncio as _asyncio
-import sys as _sys
+import asyncio
+import sys
 
 # The default event loop type was changed from the selector event loop to the proactor
 # event loop in python 3.8. To make this work on python 3.7, we need to use proactor
 # event loops--aiohttp and asyncio.subprocess don't seem to with the selector event loop
-if _sys.version_info < (3, 8):
-    _asyncio.set_event_loop_policy(_asyncio.WindowsProactorEventLoopPolicy())
+if sys.platform == "win32":
+    if sys.version_info < (3, 8):
+        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
 
 
 from meadowrun.run_job import (

meadowdata/meadowrun

commit c0f00304dcb6479aa260763dc0ada119b1b91ea2

Azure: support local code upload.

src/meadowrun/deployment_manager.py
@@ -7,7 +7,7 @@ import shutil
 import tempfile
 import urllib.parse
 import zipfile
-from typing import List, Optional, Sequence, Tuple, Union
+from typing import Any, Callable, Coroutine, List, Optional, Sequence, Tuple, Union
 
 import filelock
 
@@ -19,6 +19,7 @@ from meadowrun.aws_integration.ecr import (
 from meadowrun.aws_integration.management_lambdas.ec2_alloc_stub import (
     _MEADOWRUN_GENERATED_DOCKER_REPO,
 )
+from meadowrun.azure_integration import blob_storage
 from meadowrun.azure_integration.acr import get_acr_helper
 from meadowrun.credentials import RawCredentials, SshKey
 from meadowrun.docker_controller import (
@@ -249,18 +250,33 @@ async def _get_zip_file_code_paths(
             for zip_path in code_paths
         ]
 
-    if decoded_url.scheme == "s3":
+    async def download_and_unzip(
+        download: Callable[
+            [
+                str,
+                str,
+                str,
+            ],
+            Coroutine[Any, Any, None],
+        ]
+    ) -> List[str]:
         bucket_name = decoded_url.netloc
         object_name = decoded_url.path.lstrip("/")
         extracted_folder = os.path.join(local_copies_folder, object_name)
 
         if not os.path.exists(extracted_folder):
             zip_file_path = extracted_folder + ".zip"
-            await s3.download_file(bucket_name, object_name, zip_file_path)
+            await download(bucket_name, object_name, zip_file_path)
             with zipfile.ZipFile(zip_file_path) as zip_file:
                 zip_file.extractall(os.path.join(local_copies_folder, extracted_folder))
 
         return [os.path.join(extracted_folder, zip_path) for zip_path in code_paths]
+
+    if decoded_url.scheme == "s3":
+        return await download_and_unzip(s3.download_file)
+
+    if decoded_url.scheme == "azblob":
+        return await download_and_unzip(blob_storage.download_file)
 
     raise ValueError(f"Unknown URL scheme in {zip_file_url}")
 

src/meadowrun/run_job.py
@@ -16,6 +16,7 @@ from enum import Enum
 from typing import (
     Any,
     Callable,
+    Coroutine,
     Dict,
     Iterable,
     List,
@@ -35,6 +36,7 @@ from meadowrun.aws_integration.ec2_instance_allocation import (
     run_job_ec2_instance_registrar,
 )
 from meadowrun.aws_integration.grid_tasks_sqs import prepare_ec2_run_map
+from meadowrun.azure_integration import blob_storage
 from meadowrun.azure_integration.azure_instance_allocation import (
     run_job_azure_vm_instance_registrar,
 )
@@ -720,10 +722,11 @@ async def _prepare_code_deployment(
     if not isinstance(code_deploy, CodeZipFile):
         return code_deploy
 
-    if target == _DeploymentTarget.LOCAL:
-        return code_deploy
-    elif target == _DeploymentTarget.S3:
-        # need to deploy zip file to S3, and update the CodeZipFile
+    async def upload(
+        code_deploy: CodeZipFile,
+        ensure_upload: Callable[[str], Coroutine[Any, Any, Tuple[str, str]]],
+        scheme_name: str,
+    ) -> CodeZipFile:
         file_url = urllib.parse.urlparse(code_deploy.url)
         if file_url.scheme != "file":
             raise ValueError(f"Expected file URI: {code_deploy.url}")
@@ -733,13 +736,20 @@ async def _prepare_code_deployment(
             file_path = file_url.path[1:]
         else:
             file_path = file_url.path
-        bucket_name, object_name = await s3.ensure_uploaded(file_path)
-        s3_url = urllib.parse.urlunparse(("s3", bucket_name, object_name, "", "", ""))
-        code_deploy.url = s3_url
+        bucket_name, object_name = await ensure_upload(file_url.path)
+        code_deploy.url = urllib.parse.urlunparse(
+            (scheme_name, bucket_name, object_name, "", "", "")
+        )
         shutil.rmtree(os.path.dirname(file_path), ignore_errors=True)
         return code_deploy
+
+    if target == _DeploymentTarget.LOCAL:
+        return code_deploy
+    elif target == _DeploymentTarget.S3:
+        return await upload(code_deploy, s3.ensure_uploaded, "s3")
+
     elif target == _DeploymentTarget.AZURE_BLOB_STORAGE:
-        raise NotImplementedError("Azure Blob Storage is not implemented yet")
+        return await upload(code_deploy, blob_storage.ensure_uploaded, "azblob")
     else:
         raise ValueError(f"Unexpected value for target {target}")
 

src/meadowrun/azure_integration/azure_install_uninstall.py
@@ -1,0 +1,83 @@
+import asyncio
+from typing import Awaitable, Final
+
+from meadowrun.azure_integration import blob_storage
+from meadowrun.azure_integration.azure_mgmt_functions_setup import (
+    _create_or_update_mgmt_function_app,
+    _update_mgmt_function_code,
+)
+from meadowrun.azure_integration.mgmt_functions.azure_constants import (
+    MEADOWRUN_RESOURCE_GROUP_NAME,
+)
+from meadowrun.azure_integration.mgmt_functions.azure_core.azure_exceptions import (
+    ResourceNotFoundError,
+)
+from meadowrun.azure_integration.mgmt_functions.azure_core.azure_rest_api import (
+    azure_rest_api,
+    azure_rest_api_poll,
+    get_subscription_id,
+    wait_for_poll,
+)
+
+RESOURCE_PROVIDERS: Final = (
+    "Microsoft.OperationalInsights",
+    "microsoft.insights",
+    "Microsoft.Storage",
+    "Microsoft.ManagedIdentity",
+    "Microsoft.KeyVault",
+    "Microsoft.Network",
+    "Microsoft.Compute",
+    "Microsoft.ContainerRegistry",
+    "Microsoft.Web",
+)
+
+
+def _ensure_resource_provider_registered(
+    subscription_id: str, resource_provider_ns: str
+) -> Awaitable[None]:
+    """Ensure the given resource provider is registered."""
+    return azure_rest_api(
+        "POST",
+        f"subscriptions/{subscription_id}/providers/{resource_provider_ns}/register",
+        "2021-04-01",
+    )
+
+
+async def _register_resource_providers(subscription_id: str) -> None:
+    """Register all the resource providers we're using."""
+
+    tasks = [
+        _ensure_resource_provider_registered(subscription_id, rp)
+        for rp in RESOURCE_PROVIDERS
+    ]
+    await asyncio.gather(*tasks)
+
+
+async def delete_meadowrun_resource_group() -> None:
+    """
+    This should delete all meadowrun-generated resources as deletes everything in the
+    meadowrun resource group.
+    """
+    try:
+        # https://docs.microsoft.com/en-us/rest/api/resources/resource-groups/delete
+        await wait_for_poll(
+            await azure_rest_api_poll(
+                "DELETE",
+                f"subscriptions/{await get_subscription_id()}/resourcegroups/"
+                f"{MEADOWRUN_RESOURCE_GROUP_NAME}",
+                "2021-04-01",
+                "LocationStatusCode",
+            )
+        )
+    except ResourceNotFoundError:
+        pass
+
+
+async def install(location: str) -> None:
+    subscription_id = await get_subscription_id()
+    await _register_resource_providers(subscription_id)
+    storage_account = await _create_or_update_mgmt_function_app(
+        location, subscription_id
+    )
+    await blob_storage.ensure_container(subscription_id, storage_account, location)
+    await _update_mgmt_function_code()

src/meadowrun/azure_integration/azure_meadowrun_core.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 
 import logging
 import uuid
-from typing import Awaitable, Optional, Literal, Tuple, Set
+from typing import Optional, Literal, Tuple, Set
 
 import aiohttp
 
@@ -96,17 +96,6 @@ async def get_current_ip_address_on_vm() -> Optional[str]:
             return None
 
         return await response.text()
-
-
-def _ensure_resource_provider_registered(
-    subscription_id: str, resource_provider_ns: str
-) -> Awaitable[None]:
-    """Ensure the given resource provider is registered."""
-    return azure_rest_api(
-        "POST",
-        f"subscriptions/{subscription_id}/providers/{resource_provider_ns}/register",
-        "2021-04-01",
-    )
 
 
 _MEADOWRUN_MANAGED_IDENTITY = "meadowrun-managed-identity"
@@ -201,26 +190,6 @@ async def assign_role_to_principal(
         )
     except ResourceExistsError:
         # this means the role assignment already exists
-        pass
-
-
-async def delete_meadowrun_resource_group() -> None:
-    """
-    This should delete all meadowrun-generated resources as deletes everything in the
-    meadowrun resource group.
-    """
-    try:
-        # https://docs.microsoft.com/en-us/rest/api/resources/resource-groups/delete
-        await wait_for_poll(
-            await azure_rest_api_poll(
-                "DELETE",
-                f"subscriptions/{await get_subscription_id()}/resourcegroups/"
-                f"{MEADOWRUN_RESOURCE_GROUP_NAME}",
-                "2021-04-01",
-                "LocationStatusCode",
-            )
-        )
-    except ResourceNotFoundError:
         pass
 
 

src/meadowrun/azure_integration/azure_mgmt_functions_setup.py
@@ -10,7 +10,6 @@ import aiohttp
 import meadowrun.azure_integration.mgmt_functions
 from meadowrun.azure_integration.azure_meadowrun_core import (
     _ensure_managed_identity,
-    _ensure_resource_provider_registered,
     ensure_meadowrun_resource_group,
     ensure_meadowrun_storage_account,
     get_subscription_id,
@@ -22,6 +21,9 @@ from meadowrun.azure_integration.mgmt_functions.azure_core.azure_rest_api import
     azure_rest_api,
     azure_rest_api_poll,
     wait_for_poll,
+)
+from meadowrun.azure_integration.mgmt_functions.azure_core.azure_storage_api import (
+    StorageAccount,
 )
 from meadowrun.azure_integration.mgmt_functions.azure_constants import (
     MEADOWRUN_STORAGE_ACCOUNT_KEY_VARIABLE,
@@ -90,10 +92,12 @@ def _meadowrun_mgmt_function_app_name(subscription_id: str) -> str:
     return "mr" + subscription_id.replace("-", "")
 
 
-async def _create_or_update_mgmt_function_app(location: str) -> None:
+async def _create_or_update_mgmt_function_app(
+    location: str, subscription_id: str
+) -> StorageAccount:
     """ "Function app" and "site" seem to be interchangeable terms"""
     resource_group_path = await ensure_meadowrun_resource_group(location)
-    subscription_id = await get_subscription_id()
+
     site_path = (
         f"{resource_group_path}/providers/Microsoft.Web/sites/"
         f"{_meadowrun_mgmt_function_app_name(subscription_id)}"
@@ -102,29 +106,15 @@ async def _create_or_update_mgmt_function_app(location: str) -> None:
     # first create some prerequisites:
 
     # create application insights component for logging
-    await _ensure_resource_provider_registered(
-        subscription_id, "Microsoft.OperationalInsights"
-    )
-    await _ensure_resource_provider_registered(subscription_id, "microsoft.insights")
     app_insights_component_task = asyncio.create_task(
         _create_application_insights_component(location)
     )
-    # ensure storage account which is required by the functions runtime
-    await _ensure_resource_provider_registered(subscription_id, "Microsoft.Storage")
-    storage_account_task = ensure_meadowrun_storage_account(location, "create")
-    # managed identity for the function
-    await _ensure_resource_provider_registered(
-        subscription_id, "Microsoft.ManagedIdentity"
-    )
-    identity_id, identity_client_id = await _ensure_managed_identity(location)
 
-    # register misc resource providers we'll need while running
-    await _ensure_resource_provider_registered(subscription_id, "Microsoft.KeyVault")
-    await _ensure_resource_provider_registered(subscription_id, "Microsoft.Network")
-    await _ensure_resource_provider_registered(subscription_id, "Microsoft.Compute")
-    await _ensure_resource_provider_registered(
-        subscription_id, "Microsoft.ContainerRegistry"
-    )
+    # ensure storage account which is required by the functions runtime
+    storage_account_task = ensure_meadowrun_storage_account(location, "create")
+
+    # managed identity for the function
+    identity_id, identity_client_id = await _ensure_managed_identity(location)
 
     # now create the actual "function app"/"site"
     # https://docs.microsoft.com/en-us/rest/api/appservice/web-apps/create-or-update
@@ -132,7 +122,6 @@ async def _create_or_update_mgmt_function_app(location: str) -> None:
     # azure_rest_api_poll(poll_scheme="GetProvisioningState"), but there's no
     # "provisioningState" property to check. This seems like a bug/outdated
     # implementation in the SDK.
-    await _ensure_resource_provider_registered(subscription_id, "Microsoft.Web")
     await azure_rest_api(
         "PUT",
         site_path,
@@ -190,6 +179,7 @@ async def _create_or_update_mgmt_function_app(location: str) -> None:
             }
         },
     )
+    return storage_account
 
 
 _IGNORE_FOR_ZIP = {".git", ".venv", ".vscode"}
@@ -283,18 +273,3 @@ async def _retry(
             else:
                 print(f"{retry_message}: {e}")
                 await asyncio.sleep(delay_seconds)
-
-
-async def create_or_update_mgmt_function(location: str) -> None:
-    await _create_or_update_mgmt_function_app(location)
-    # Sometimes, even though the previous call has completed, the
-    # _update_mgmt_function_code call will fail in a weird way--the initial call will
-    # succeed, but then polling for the status of the deployment will throw a 400, so we
-    # retry it.
-    await _retry(
-        _update_mgmt_function_code,
-        Exception,
-        3,
-        5,
-        "Waiting for Azure function creation to complete",
-    )

src/meadowrun/azure_integration/blob_storage.py
@@ -1,0 +1,111 @@
+import hashlib
+import os
+from typing import Tuple
+
+from meadowrun.azure_integration.azure_meadowrun_core import (
+    ensure_meadowrun_storage_account,
+)
+from meadowrun.azure_integration.mgmt_functions.azure_core.azure_exceptions import (
+    AzureRestApiError,
+)
+from meadowrun.azure_integration.mgmt_functions.azure_core.azure_rest_api import (
+    azure_rest_api,
+)
+from meadowrun.azure_integration.mgmt_functions.azure_core.azure_storage_api import (
+    StorageAccount,
+    azure_blob_api,
+)
+from meadowrun.azure_integration.mgmt_functions.azure_constants import (
+    MEADOWRUN_RESOURCE_GROUP_NAME,
+)
+
+
+CONTAINER_NAME = "meadowrun"
+
+
+_LIFECYCLE_POLICY = {
+    "rules": [
+        {
+            "enabled": True,
+            "name": "meadowrun-lifecycle-policy",
+            "type": "Lifecycle",
+            "definition": {
+                "actions": {
+                    "baseBlob": {"delete": {"daysAfterModificationGreaterThan": 14}}
+                },
+                "filters": {"blobTypes": ["blockBlob"], "prefixMatch": ["meadowrun/"]},
+            },
+        }
+    ]
+}
+
+
+async def ensure_container(
+    subscription_id: str, storage_account: StorageAccount, location: str
+) -> None:
+    storage_account = await ensure_meadowrun_storage_account(location, "create")
+    # https://docs.microsoft.com/en-us/rest/api/storageservices/create-container
+    await azure_blob_api(
+        "PUT",
+        storage_account,
+        CONTAINER_NAME,
+        query_parameters={"restype": "container"},
+        ignored_status_codes=((409, "ContainerAlreadyExists"),),
+    )
+
+    await azure_rest_api(
+        "PUT",
+        f"subscriptions/{subscription_id}/resourceGroups/"
+        f"{MEADOWRUN_RESOURCE_GROUP_NAME}/providers/Microsoft.Storage/storageAccounts/"
+        f"{storage_account.name}/managementPolicies/default",
+        api_version="2021-09-01",
+        json_content={"properties": {"policy": _LIFECYCLE_POLICY}},
+    )
+
+
+async def ensure_uploaded(file_path: str) -> Tuple[str, str]:
+    hasher = hashlib.blake2b()
+    with open(file_path, "rb") as file:
+        buf = file.read()
+        hasher.update(buf)
+    digest = hasher.hexdigest()
+
+    # the empty location is inelegant here, but:
+    # 1. it's only used if the storage account does not exist yet; in that case we're
+    #    not even getting to this point
+    # 2. it's pretty annoying to thread it where download_file is used.
+    storage_account = await ensure_meadowrun_storage_account("", on_missing="raise")
+    try:
+        _ = await azure_blob_api("HEAD", storage_account, f"{CONTAINER_NAME}/{digest}")
+        # CONTAINER_NAME is a constant on Azure, but not AWS. Returning it here for
+        # consistency.
+        return CONTAINER_NAME, digest
+    except AzureRestApiError as error:
+        if error.status != 404:
+            raise error
+
+    # doesn't exist, need to upload it
+    content_length = os.path.getsize(file_path)
+    with open(file_path, "rb") as file:
+        _ = await azure_blob_api(
+            "PUT",
+            storage_account,
+            f"{CONTAINER_NAME}/{digest}",
+            additional_headers={
+                "Content-Length": str(content_length),
+                "x-ms-blob-type": "BlockBlob",
+            },
+            binary_content=file,
+        )
+    return CONTAINER_NAME, digest
+
+
+async def download_file(container_name: str, object_name: str, file_name: str) -> None:
+    # see comment in ensure_uploaded regarding empty location
+    storage_account = await ensure_meadowrun_storage_account("", on_missing="raise")
+
+    result: bytes = await azure_blob_api(
+        "GET", storage_account, f"{container_name}/{object_name}"
+    )
+    with open(file_name, "wb") as file:
+        file.write(result)

src/meadowrun/azure_integration/mgmt_functions/azure_core/azure_exceptions.py
@@ -1,5 +1,5 @@
 import xml.dom.minidom
-from typing import Optional, Any, Tuple, Type
+from typing import Iterable, Optional, Any, Tuple, Type
 
 import aiohttp
 import requests
@@ -84,7 +84,10 @@ def _exception_type_from_code(code: Optional[str]) -> Type:
         return AzureRestApiError
 
 
-async def raise_for_status(response: aiohttp.ClientResponse) -> None:
+async def raise_for_status(
+    response: aiohttp.ClientResponse,
+    ignored_status_codes: Iterable[Tuple[int, str]] = tuple(),
+) -> None:
     """
     Like response.raise_for_status, but raises AzureRestApiError based on parsing the
     response content
@@ -118,7 +121,8 @@ async def raise_for_status(response: aiohttp.ClientResponse) -> None:
                 response_text = await response.text()
             message = response_text
 
-        raise _exception_type_from_code(code)(response.status, code, message)
+        if code is None or (response.status, code) not in ignored_status_codes:
+            raise _exception_type_from_code(code)(response.status, code, message)
 
 
 def raise_for_status_sync(response: requests.Response) -> None:

commit af37df553af855d0eb59675a27ab57315443141f

Minor fixes and republishing as 0.1.11 - Adding encoding="utf-8" to a bunch of open calls. Immediate cause is that a python package's description which goes into poetry.lock has added an emoji - Fixing a regression in c0f00304 that prevents local code upload from working on Windows

build_scripts/conda-recipe/meta.yaml
@@ -5,9 +5,9 @@
 # 1. Update this file to be in sync with pyproject.toml, see the comments in this file
 #    for what needs to be synced from pyproject.toml
 # 2. Build the pip package using `poetry build`
-# 3. Unpack the resulting meadowrun-0.1.10.tar.gz file into /dist/meadowrun-0.1.10 with
-#    `tar xzvf dist\meadowrun-0.1.10.tar.gz -C dist` (E.g. setup.py should be at
-#    /dist/meadowrun-0.1.10/setup.py)
+# 3. Unpack the resulting meadowrun-0.1.11.tar.gz file into /dist/meadowrun-0.1.11 with
+#    `tar xzvf dist\meadowrun-0.1.11.tar.gz -C dist` (E.g. setup.py should be at
+#    /dist/meadowrun-0.1.11/setup.py)
 # 4. Now run `conda build -c defaults -c conda-forge --python 3.9
 #    build_scripts\conda-recipe`
 
@@ -16,15 +16,15 @@
 #     (obviously replace c:\bin\Miniconda with your conda installation)
 
 # 5b. To publish this image, run `conda activate base` then `anaconda upload
-#     C:\bin\Miniconda\conda-bld\noarch\meadowrun-0.1.10-py_0.tar.bz2`. If you're running
+#     C:\bin\Miniconda\conda-bld\noarch\meadowrun-0.1.11-py_0.tar.bz2`. If you're running
 #     this for the first time, you'll need to run `anaconda login` before you run
 #     `anaconda upload`
 
 package:
   name: meadowrun
-  version: 0.1.10  # from pyproject.toml
+  version: 0.1.11  # from pyproject.toml
 source:
-  path: ../../dist/meadowrun-0.1.10  # from pyproject.toml
+  path: ../../dist/meadowrun-0.1.11  # from pyproject.toml
 build:
   entry_points:  # from pyproject.toml
     - meadowrun-local = meadowrun.run_job_local_main:command_line_main

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "meadowrun"
-version = "0.1.10"
+version = "0.1.11"
 description = "The easiest way to run python code on one or more remote machines"
 authors = ["Richard Lee <hrichardlee@gmail.com>"]
 homepage = "https://github.com/meadowdata/meadowrun"

src/meadowrun/aws_integration/ec2_instance_allocation.py
@@ -46,15 +46,15 @@ from meadowrun.run_job_core import AllocCloudInstancesInternal, JobCompletion, S
 # AMIs that have meadowrun pre-installed. These are all identical, we just need to
 # replicate into each region.
 _EC2_ALLOC_AMIS = {
-    "us-east-2": "ami-0d80e2dadd8439c5a",
-    "us-east-1": "ami-0df439021f39e35c5",
-    "us-west-1": "ami-0f17cb35f9125be60",
-    "us-west-2": "ami-020f618924d9ecefb",
-    "eu-central-1": "ami-0091f4d1b91bc6037",
-    "eu-west-1": "ami-085312fdf180a5c22",
-    "eu-west-2": "ami-008b289f344811727",
-    "eu-west-3": "ami-005ad713537ccf4dd",
-    "eu-north-1": "ami-048a4a99b33ffae3b",
+    "us-east-2": "ami-07653668da87ca748",
+    "us-east-1": "ami-023ef6195ecb8e648",
+    "us-west-1": "ami-032b1753347534924",
+    "us-west-2": "ami-074513d806ac4d30e",
+    "eu-central-1": "ami-068c10b14043c89d0",
+    "eu-west-1": "ami-0e5d382309d0a5906",
+    "eu-west-2": "ami-05dba868373463124",
+    "eu-west-3": "ami-0f8a2de09fa92ae08",
+    "eu-north-1": "ami-024bb642686e9e0ca",
 }
 
 

src/meadowrun/aws_integration/ec2_pricing.py
@@ -38,7 +38,7 @@ def _get_region_description_for_pricing(region_code: str) -> str:
     region_code, but the pricing API weirdly uses description.
     """
     endpoint_file = resource_filename("botocore", "data/endpoints.json")
-    with open(endpoint_file, "r") as f:
+    with open(endpoint_file, "r", encoding="utf-8") as f:
         data = json.load(f)
     # Botocore is using Europe while Pricing API using EU...sigh...
     return data["partitions"][0]["regions"][region_code]["description"].replace(

src/meadowrun/aws_integration/ec2_ssh_keys.py
@@ -103,5 +103,5 @@ def get_meadowrun_ssh_key(region_name: str) -> paramiko.PKey:
 
 def download_ssh_key(output_path: str, region_name: str) -> None:
     private_key_text = _get_meadowrun_ssh_key_text(region_name)
-    with open(output_path, "w") as output_file:
+    with open(output_path, "w", encoding="utf-8") as output_file:
         output_file.write(private_key_text)

src/meadowrun/azure_integration/azure_ssh_keys.py
@@ -211,5 +211,5 @@ async def download_ssh_key(output_path: str, location: str) -> None:
     if key is None:
         raise ValueError("Azure SSH key secret's value was None")
 
-    with open(output_path, "w") as output_file:
+    with open(output_path, "w", encoding="utf-8") as output_file:
         output_file.write(key)

src/meadowrun/azure_integration/azure_vms.py
@@ -43,7 +43,7 @@ _MEADOWRUN_COMMUNITY_IMAGE_ID = (
     "/CommunityGalleries/meadowprodeastus-e8b60fd5-8978-467b-a1b0-5b83cbf5393d/Images/"
     "meadowrun"
 )
-_MEADOWRUN_IMAGE_VERSION = "0.1.10"
+_MEADOWRUN_IMAGE_VERSION = "0.1.11"
 
 
 async def _ensure_virtual_network_and_subnet(location: str) -> str:

src/meadowrun/deployment_manager.py
@@ -421,9 +421,13 @@ def _get_path_and_hash(
             spec_hash = hash_spec(environment_spec.spec_lock.encode("UTF-8"))
             path_to_spec = os.path.join(interpreter_spec_path, spec_hash)
             os.makedirs(path_to_spec, exist_ok=True)
-            with open(os.path.join(path_to_spec, "pyproject.toml"), "w") as spec_file:
+            with open(
+                os.path.join(path_to_spec, "pyproject.toml"), "w", encoding="utf-8"
+            ) as spec_file:
                 spec_file.write(environment_spec.spec)
-            with open(os.path.join(path_to_spec, "poetry.lock"), "w") as lock_file:
+            with open(
+                os.path.join(path_to_spec, "poetry.lock"), "w", encoding="utf-8"
+            ) as lock_file:
                 lock_file.write(environment_spec.spec_lock)
         else:
             spec_hash = hash_spec(environment_spec.spec.encode("UTF-8"))
@@ -438,7 +442,7 @@ def _get_path_and_hash(
                 )
 
             path_to_spec = os.path.join(interpreter_spec_path, spec_filename)
-            with open(path_to_spec, "w") as env_spec:
+            with open(path_to_spec, "w", encoding="utf-8") as env_spec:
                 env_spec.write(environment_spec.spec)
 
         return path_to_spec, spec_hash

src/meadowrun/run_job.py
@@ -391,9 +391,9 @@ async def _get_current_local_interpreter() -> InterpreterDeployment:
     # reason this isn't the case, we'll fall through to the pip-based case, which will
     # mostly work for poetry environments.
     if os.path.isfile("pyproject.toml") and os.path.isfile("poetry.lock"):
-        with open("pyproject.toml") as project_file:
+        with open("pyproject.toml", encoding="utf-8") as project_file:
             project_file_contents = project_file.read()
-        with open("poetry.lock") as lock_file:
+        with open("poetry.lock", encoding="utf-8") as lock_file:
             lock_file_contents = lock_file.read()
         print("Mirroring current poetry environment")
         return EnvironmentSpec(
@@ -462,13 +462,13 @@ class Deployment:
                 InterpreterDeployment, VersionedInterpreterDeployment
             ] = await _get_current_local_interpreter()
         elif isinstance(interpreter, CondaEnvironmentYmlFile):
-            with open(interpreter.path_to_yml_file) as f:
+            with open(interpreter.path_to_yml_file, encoding="utf-8") as f:
                 interpreter_spec = EnvironmentSpec(
                     environment_type=EnvironmentType.CONDA,
                     spec=f.read(),
                 )
         elif isinstance(interpreter, PipRequirementsFile):
-            with open(interpreter.path_to_requirements_file) as f:
+            with open(interpreter.path_to_requirements_file, encoding="utf-8") as f:
                 interpreter_spec = EnvironmentSpec(
                     environment_type=EnvironmentType.PIP,
                     spec=f.read(),
@@ -737,7 +737,7 @@ async def _prepare_code_deployment(
             file_path = file_url.path[1:]
         else:
             file_path = file_url.path
-        bucket_name, object_name = await ensure_upload(file_url.path)
+        bucket_name, object_name = await ensure_upload(file_path)
         code_deploy.url = urllib.parse.urlunparse(
             (scheme_name, bucket_name, object_name, "", "", "")
         )

src/meadowrun/run_job_local.py
@@ -132,7 +132,7 @@ def _prepare_py_command(
         result_path_container = f"{MEADOWRUN_IO_MOUNT_LINUX}/{job.job_id}.result"
         # we create an empty file here so that we can expose it to the docker container,
         # docker does not let us bind non-existent files
-        open(result_path, "w").close()
+        open(result_path, "w", encoding="utf-8").close()
     else:
         result_path_container = result_path
     io_files.append(job.job_id + ".result")
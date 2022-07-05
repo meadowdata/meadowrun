86a863d725b956ffabc5bd7e5aa89a1503011b6b
Hyunho Richard Lee <hrichardlee@gmail.com>
2022-07-05 05:35:41 -0400

    Publishing 0.1.13

---
+++ build_scripts/build_conda.bat
@@ -0,0 +1,21 @@
+REM Before running this file, update meta.yaml to be in sync with pyproject.toml, see
+REM comments in that file to see what needs to be synced from pyproject.toml. Also
+REM update the version numbers in this file
+
+REM Build the pip package
+poetry build
+
+REM Unpack the resulting meadowrun-<version>.tar.gz file into /dist/meadowrun-<version>
+tar xzvf dist\meadowrun-0.1.13.tar.gz -C dist
+
+REM Build the conda package
+call conda build -c defaults -c conda-forge --python 3.9 build_scripts\conda-recipe
+
+REM To test this, you can install this package into a conda environment by running
+REM `conda install meadowrun -c c:\bin\Miniconda\conda-bld -c defaults -c conda-forge`
+REM (obviously replace c:\bin\Miniconda with your conda installation)
+
+REM To publish this image, run
+REM `conda activate base && anaconda upload C:\bin\Miniconda\conda-bld\noarch\meadowrun-0.1.13-py_0.tar.bz2`
+REM If you're running this for the first time, you'll need to run `anaconda login`
+REM before you run `anaconda upload`

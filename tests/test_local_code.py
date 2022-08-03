meadowdata/meadowrun

commit be3d8bf22869b2a9c8c597cadb4ec4d237d3b6fe

Add support for specifying non python files - Change "additional_paths" to "working_directory_globs". Including non python files can only happen relative to the current working directory now. Any file types can be included. - Fixes an obscure "bug" where if the current working directory was not on the python path, but subdirectories of the current working directory were on the python path, then .py files in the current directory would get picked up unnecessarily - Automatically pick up .pyd files in python paths

src/meadowrun/local_code.py
@@ -1,46 +1,33 @@
+import glob
 import itertools
 import os
-from os.path import realpath, join, splitext
 import sys
 import uuid
 import zipfile
-from typing import Dict, Iterable, List, Set, Tuple
+from os.path import realpath, join, splitext
+from typing import Iterable, List, Set, Tuple
 
 
 def zip_local_code(
     result_zip_dir: str,
     include_sys_path: bool = True,
     additional_python_paths: Iterable[str] = tuple(),
-    additional_non_python_paths: Iterable[str] = tuple(),
-    include_extensions: Iterable[str] = (".py",),
-) -> Tuple[str, List[str], List[str]]:
+    python_paths_extensions: Iterable[str] = (".py", ".pyd"),
+    working_directory_globs: Iterable[str] = tuple(),
+) -> Tuple[str, List[str], str]:
     """
     The goal of this function is to zip up "the current local python code excluding the
-    interpreter code" as well as any additional non-python files that might be needed to
-    run code remotely.
+    interpreter code" as well as any specified files in the current working directory.
 
     There are two types of folders/files that make it into the zip file. The first are
     "python paths". Assuming include_sys_path is True, "python paths" will be
     additional_python_paths + paths in sys.path that are NOT part of the interpreter,
-    i.e. paths that start with sys.prefix, etc. The second are "non-python paths". These
-    two types of paths are treated exactly the same in the zip file. The only difference
-    between these two types of paths is that we keep track of them separately in the
-    return value--the returned tuple will include a list of "python paths" as they
-    appear in the zip file and "non-python paths" as they appear in the zip file.
+    i.e. paths that do NOT start with sys.prefix, etc. The second type are files
+    specified by working_directory_globs which are always relative to the current
+    working directory.
 
-    Non-python paths are useful for two reasons:
-    - One is to add structure to the zip file. This function will consolidate paths that
-      share a prefix. So for example, if `src/foo` and `src/bar/baz` are python paths,
-      but `src` itself is not a python path, you can set `src` as a non-python path and
-      your zip file will have the right structure. If you don't do this, `foo` and `baz`
-      will just be top-level directories in the zip file.
-    - You'll usually want to include the current working directory as a "non-python
-      path" if it's not already a python path. Then when you unzip, you can set the
-      working directory to the corresponding unzipped directory and relative paths will
-      work as expected.
-
-    Both python paths and non-python paths will only include *.py files (or whatever is
-    specified in include_extensions).
+    python paths will only include *.py and *.pyd files (or whatever is specified in
+    python_paths_extensions).
 
     Args:
         result_zip_dir: directory where to put the resulting zip file.
@@ -48,30 +35,47 @@ def zip_local_code(
             sys.prefix or sys.base_prefix are automatically excluded. Defaults to True.
         additional_python_paths: Additional python paths to zip. Defaults to empty
             tuple, i.e. no additional python paths.
-        additional_non_python_paths: Additional paths to zip. Defaults to empty tuple,
-            i.e. no additional non-python paths.
-        include_extensions: Only files with these extensions are zipped. Defaults to
-            (".py",), i.e. only python files.
+        python_paths_extensions: Only files with these extensions are zipped. Defaults
+            to (".py", ".pyd"), i.e. only python files.
+        working_directory_globs: Globs that will be passed directly to glob.glob to
+            specify any file relative to the current working directory. Globs will only
+            match files.
 
     Raises:
         ValueError: If there are no paths to zip.
 
     Returns:
-        Tuple[str, List[str], List[str]]: path to zip file in result_zip_dir, python
-        paths in the zip file, non-python paths in the zip file
+        Tuple[str, List[str], str]: path to zip file in result_zip_dir, python paths in
+        the zip file, current working directory in the zip file
     """
+    current_working_directory = realpath(os.getcwd())
+    non_python_paths_to_zip = [current_working_directory]
+    current_working_directory_parent = os.path.dirname(current_working_directory)
+    # this will hold (path_to_file, path_to_containing_dir) for every file that matches
+    # a glob
+    real_file_paths_to_zip = []
+    for file_glob in working_directory_globs:
+        for file in glob.glob(file_glob, recursive=True):
+            if os.path.isfile(file):
+                file_real_path = realpath(file)
+                if not file_real_path.startswith(current_working_directory_parent):
+                    raise ValueError(
+                        "Specifying files outside of the current working directory's "
+                        "parent is not supported. I.e. ../foo is fine, but ../../foo "
+                        "will not work"
+                    )
+                dir_real_path = os.path.dirname(file_real_path)
+                real_file_paths_to_zip.append((file_real_path, dir_real_path))
+                non_python_paths_to_zip.append(dir_real_path)
 
-    python_paths_to_zip, non_python_paths_to_zip = _get_paths_to_zip(
-        additional_python_paths, include_sys_path, additional_non_python_paths
+    real_python_paths_to_zip = _get_python_paths_to_zip(
+        additional_python_paths, include_sys_path
     )
-    if not python_paths_to_zip and not non_python_paths_to_zip:
-        raise ValueError("No paths to zip")
 
     (
-        real_paths_to_zip_paths,
         python_zip_paths,
         non_python_zip_paths,
-    ) = _consolidate_paths_to_zip(python_paths_to_zip, non_python_paths_to_zip)
+    ) = _consolidate_paths_to_zip(real_python_paths_to_zip, non_python_paths_to_zip)
 
     # Currently we always zip all the files and then compute the hash. The hash is just
     # used to avoid upload to S3. In future we could try to split the files into chunks
@@ -80,23 +84,28 @@ def zip_local_code(
     # building, so this is not currently a bottleneck.
     zip_file_path = join(result_zip_dir, str(uuid.uuid4()) + ".zip")
     with zipfile.ZipFile(zip_file_path, "w") as zip_file:
-        for real_path, zip_path in real_paths_to_zip_paths.items():
+        for real_path, zip_path in zip(real_python_paths_to_zip, python_zip_paths):
             for dirpath, _, filenames in os.walk(real_path):
                 for filename in filenames:
-                    ext = splitext(filename)[1]
-                    if ext in include_extensions:
+                    if splitext(filename)[1].lower() in python_paths_extensions:
                         full = join(dirpath, filename)
                         full_zip = full.replace(real_path, zip_path, 1)
                         zip_file.write(full, full_zip)
 
-    return zip_file_path, python_zip_paths, non_python_zip_paths
+        for (file_real_path, dir_real_path), dir_zip_path in zip(
+            real_file_paths_to_zip, itertools.islice(non_python_zip_paths, 1, None)
+        ):
+            zip_file.write(
+                file_real_path, file_real_path.replace(dir_real_path, dir_zip_path, 1)
+            )
+
+    return zip_file_path, python_zip_paths, non_python_zip_paths[0]
 
 
-def _get_paths_to_zip(
+def _get_python_paths_to_zip(
     python_paths_to_zip: Iterable[str],
     include_sys_path: bool,
-    non_python_paths_to_zip: Iterable[str],
-) -> Tuple[List[str], List[str]]:
+) -> List[str]:
     python_paths_to_zip = [realpath(path) for path in python_paths_to_zip]
     if include_sys_path:
         for candidate in sys.path:
@@ -109,13 +118,12 @@ def _get_paths_to_zip(
                     python_paths_to_zip.append(realpath(os.getcwd()))
                 else:
                     python_paths_to_zip.append(realpath(candidate))
-    non_python_paths_to_zip = [realpath(path) for path in non_python_paths_to_zip]
-    return python_paths_to_zip, non_python_paths_to_zip
+    return python_paths_to_zip
 
 
 def _consolidate_paths_to_zip(
     real_python_paths_to_zip: Iterable[str], real_non_python_paths_to_zip: Iterable[str]
-) -> Tuple[Dict[str, str], List[str], List[str]]:
+) -> Tuple[List[str], List[str]]:
     """Generates a minimal dictionary of real paths that need to be zipped, to the paths
     in the zip file.
     """
@@ -150,9 +158,14 @@ def _consolidate_paths_to_zip(
     for candidate in root_candidates:
         if candidate.startswith(current_root):
             # we have a subpath. Record it.
-            real_paths_to_zip_paths[candidate] = candidate.replace(
+            zip_path = candidate.replace(
                 current_root, real_paths_to_zip_paths[current_root], 1
             )
+            if os.path.sep != "/":
+                # we have to make the sub-paths compatible with Linux which is our
+                # target OS
+                zip_path = zip_path.replace(os.path.sep, "/")
+            real_paths_to_zip_paths[candidate] = zip_path
         else:
             # we have a new root.
             current_root = candidate
@@ -169,10 +182,4 @@ def _consolidate_paths_to_zip(
         real_paths_to_zip_paths[real_path] for real_path in real_non_python_paths_to_zip
     ]
 
-    # keeping just the root paths - these are the only ones that need to be zipped
-    root_paths_to_zip_paths = {
-        root: zip_path
-        for root, zip_path in real_paths_to_zip_paths.items()
-        if root in real_root_paths
-    }
-    return root_paths_to_zip_paths, zip_python_paths, zip_non_python_paths
+    return zip_python_paths, zip_non_python_paths

tests/test_local_code.py
@@ -7,26 +7,23 @@ from meadowrun import local_code
 
 
 @pytest.mark.parametrize(
-    "python_paths,other_paths,rel_to_local,expected_python_paths,expected_other_paths",
+    "python_paths,other_paths,expected_python_paths,expected_other_paths",
     [
         (  # simplest case: one root with a few subfolders
             ["/usr/lib", "/usr/lib/python", "/usr/lib/python/more"],
             [],
-            {"/usr/lib": "lib"},
             ["lib", "lib/python", "lib/python/more"],
             [],
         ),
         (  # two roots each with a subfolder
             ["/usr/lib", "/usr/lib/python", "/home/foo/mdr", "/home/foo/mdr/src"],
             [],
-            {"/usr/lib": "lib", "/home/foo/mdr": "mdr"},
             ["lib", "lib/python", "mdr", "mdr/src"],
             [],
         ),
         (  # two roots without subfolders, but same name
             ["/home/user/lib2/src", "/home/user/lib1/src"],
             [],
-            {"/home/user/lib1/src": "src", "/home/user/lib2/src": "src_0"},
             ["src_0", "src"],
             [],
         ),
@@ -38,7 +35,6 @@ from meadowrun import local_code
                 "/home/user/lib2/src/bar",
             ],
             [],
-            {"/home/user/lib1/src": "src", "/home/user/lib2/src": "src_0"},
             ["src_0", "src", "src/foo", "src_0/bar"],
             [],
         ),
@@ -46,60 +42,182 @@ from meadowrun import local_code
         (  # simplest case: one root with a few subfolders
             ["/usr/lib/python", "/usr/lib/python/more"],
             ["/usr/lib"],
-            {"/usr/lib": "lib"},
             ["lib/python", "lib/python/more"],
             ["lib"],
         ),
         (  # two roots each with a subfolder
             ["/usr/lib", "/usr/lib/python"],
             ["/home/foo/mdr", "/home/foo/mdr/src"],
-            {"/usr/lib": "lib", "/home/foo/mdr": "mdr"},
             ["lib", "lib/python"],
             ["mdr", "mdr/src"],
         ),
         (  # two roots without subfolders, but same name
             ["/home/user/lib2/src"],
             ["/home/user/lib1/src"],
-            {"/home/user/lib1/src": "src", "/home/user/lib2/src": "src_0"},
             ["src_0"],
             ["src"],
         ),
         (  # two roots with subfolders, same name
             ["/home/user/lib2/src", "/home/user/lib2/src/bar"],
             ["/home/user/lib1/src", "/home/user/lib1/src/foo"],
-            {"/home/user/lib1/src": "src", "/home/user/lib2/src": "src_0"},
             ["src_0", "src_0/bar"],
             ["src", "src/foo"],
         ),
     ],
 )
 def test_consolidate_paths(
-    python_paths, other_paths, rel_to_local, expected_python_paths, expected_other_paths
+    python_paths, other_paths, expected_python_paths, expected_other_paths
 ):
     (
-        actual_rel_to_local,
         actual_python_paths,
         actual_other_paths,
     ) = local_code._consolidate_paths_to_zip(python_paths, other_paths)
     assert len(actual_python_paths) == len(python_paths)
     assert len(actual_other_paths) == len(other_paths)
 
-    assert rel_to_local == actual_rel_to_local
     assert expected_python_paths == actual_python_paths
     assert expected_other_paths == actual_other_paths
 
 
 def test_zip_file():
     with tempfile.TemporaryDirectory() as temp:
-        result = local_code.zip_local_code(
-            temp, additional_non_python_paths=[os.getcwd()]
-        )
+        result = local_code.zip_local_code(temp)
         assert result is not None
-        zip_file, zip_python_paths, extra_paths = result
-        assert len(extra_paths) == 1
+        zip_file, zip_python_paths, cwd = result
         with zipfile.ZipFile(zip_file) as zip:
-            all_paths = zip_python_paths + extra_paths
+            all_paths = zip_python_paths + [cwd]
             for file in zip.filelist:
                 ext = os.path.splitext(file.filename)[1]
                 assert ext == ".py"
                 assert any(zip_path in file.filename for zip_path in all_paths)
+
+
+@pytest.mark.parametrize(
+    "available_files,working_directory,working_directory_globs,python_paths,"
+    "expected_files_in_zip,expected_python_paths_in_zip,"
+    "expected_working_directory_in_zip",
+    [
+        # root is not on python path, but subdirectories are. Python files outside of
+        # the subdirectories should be ignored. Also, non-.py files are ignored
+        (
+            [
+                "root/a/b/x1.py",
+                "root/a/c/x2.py",
+                "root/a/x3.py",
+                "root/x4.py",
+                "root/a/x5.txt",
+                "root/a/b/x6.txt",
+            ],
+            "root",
+            (),
+            ["root/a/b", "root/a/c"],
+            ["root/a/b/x1.py", "root/a/c/x2.py"],
+            ["root/a/b", "root/a/c"],
+            "root",
+        ),
+        # simple case of include **/*.txt files
+        (
+            [
+                "root/a/x1.py",
+                "root/a/x2.txt",
+                "root/b/x3.py",
+                "root/b/x4.txt",
+                "root/x5.txt",
+            ],
+            "root",
+            ["**/*.txt"],
+            ["root"],
+            [
+                "root/a/x1.py",
+                "root/a/x2.txt",
+                "root/b/x3.py",
+                "root/b/x4.txt",
+                "root/x5.txt",
+            ],
+            ["root"],
+            "root",
+        ),
+        # same as above, but only include txt files in b
+        (
+            [
+                "root/a/x1.py",
+                "root/a/x2.txt",
+                "root/b/x3.py",
+                "root/b/x4.txt",
+                "root/x5.txt",
+            ],
+            "root",
+            ["b/**/*.txt"],
+            ["root"],
+            ["root/a/x1.py", "root/b/x3.py", "root/b/x4.txt"],
+            ["root"],
+            "root",
+        ),
+        # reference files outside of root
+        (
+            ["root/x1.py", "root/x2.txt", "other_root/x3.txt"],
+            "root",
+            ["../other_root/**/*.txt"],
+            ["root"],
+            ["root/x1.py", "other_root/x3.txt"],
+            ["root"],
+            "root",
+        ),
+    ],
+)
+def test_zip_local_code(
+    available_files,
+    working_directory,
+    working_directory_globs,
+    python_paths,
+    expected_files_in_zip,
+    expected_python_paths_in_zip,
+    expected_working_directory_in_zip,
+):
+    with tempfile.TemporaryDirectory() as temp_source, tempfile.TemporaryDirectory() as temp_dest:  # noqa: E501
+        # create the specified files
+        for file in available_files:
+            file_path = os.path.join(temp_source, file)
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+            with open(file_path, mode="w", encoding="utf-8") as f:
+                f.write("Hello!")
+
+        orig_dir = os.getcwd()
+        try:
+            os.chdir(os.path.join(temp_source, working_directory))
+
+            (
+                result_zip_file,
+                python_paths_in_zip,
+                working_directory_in_zip,
+            ) = local_code.zip_local_code(
+                temp_dest,
+                False,
+                additional_python_paths=[
+                    os.path.join(temp_source, p) for p in python_paths
+                ],
+                working_directory_globs=working_directory_globs,
+            )
+
+            with zipfile.ZipFile(result_zip_file, "r") as zf:
+                assert set(zf.namelist()) == set(expected_files_in_zip)
+
+            assert python_paths_in_zip == expected_python_paths_in_zip
+            assert working_directory_in_zip == expected_working_directory_in_zip
+        finally:
+            os.chdir(orig_dir)
+
+
+def test_zip_local_code_exception():
+    with pytest.raises(ValueError, match="outside of the current working directory"):
+        # reference files two folders outside of root does not work
+        test_zip_local_code(
+            ["nested/root/x1.py", "nested/root/x2.txt", "other_root/x3.txt"],
+            "nested/root",
+            ["../../other_root/**/*.txt"],
+            ["nested/root"],
+            # this is what would need to be returned to support this scenario
+            ["nested/root/x1.py", "other_root/x3.txt"],
+            ["nested/root"],
+            "nested/root",
+        )

src/meadowrun/run_job.py
@@ -487,6 +487,7 @@ class Deployment:
         interpreter: Union[
             LocalInterpreter, InterpreterSpecFile, ContainerInterpreterBase, None
         ] = None,
+        working_directory_globs: Union[str, Iterable[str], None] = None,
         environment_variables: Optional[Dict[str, str]] = None,
     ) -> Deployment:
         """A deployment that mirrors the local environment and code.
@@ -505,6 +506,18 @@ class Deployment:
                 [CondaEnvironmentYmlFile][meadowrun.CondaEnvironmentYmlFile],
                 [PipRequirementsFile][meadowrun.PipRequirementsFile],
                 [PoetryProjectPath][meadowrun.PoetryProjectPath]
+            working_directory_globs: Most of the time, your current working directory
+                will be on `sys.path`, and so any *.py files from your current working
+                directory will be uploaded by `mirror_local`. However, any other types
+                of files will be ignored by default. You can specify files to include
+                from your current working directory with this argument. Examples:
+                `"foo/bar.txt"` will include the specified file. `"*.txt"` will specify
+                txt files in your current directory (but not recursively). `"**/*.txt"`
+                will specify all txt files in your current directory recursively (e.g.
+                will capture both `1.txt` and `foo/2.txt`). `"foo/**/*.txt"` will
+                capture all txt files in the foo directory. If you specify files like
+                this, you will be able to access them with relative paths in the remote
+                code the same way you reference them locally.
             environment_variables: e.g. `{"PYTHONHASHSEED": "0"}`. These environment
                 variables will be set in the remote environment.
 
@@ -576,11 +589,19 @@ class Deployment:
         else:
             raise ValueError(f"Unexpected type of interpreter {type(interpreter)}")
 
+        if working_directory_globs is None:
+            working_directory_globs = ()
+        elif isinstance(working_directory_globs, str):
+            working_directory_globs = [working_directory_globs]
+
         # annoyingly, this tmp dir now gets deleted in run_local when the file
         # has been uploaded/unpacked depending on the Host implementation
         tmp_dir = tempfile.mkdtemp()
-        zip_file_path, zip_python_paths, [zip_cwd] = local_code.zip_local_code(
-            tmp_dir, include_sys_path, additional_python_paths, [os.getcwd()]
+        zip_file_path, zip_python_paths, zip_cwd = local_code.zip_local_code(
+            tmp_dir,
+            include_sys_path,
+            additional_python_paths,
+            working_directory_globs=working_directory_globs,
         )
 
         url = urllib.parse.urlunparse(("file", "", zip_file_path, "", "", ""))

src/meadowrun/run_job_local.py
@@ -471,6 +471,11 @@ async def _launch_container_job(
         unique_code_paths = list(code_paths)
 
     # populate binds with code paths we need
+    # TODO this isn't exactly right--some code paths "overlap", like foo/a and foo/a/b.
+    # We should just bind foo/a once as e.g. /meadowrun/code0, and then add both
+    # /meadowrun/code0 and /meadowrun/code0/b to the PYTHONPATH. Instead, right now, we
+    # mount /foo/a as /meadowrun/code0 and /foo/a/b as /meadowrun/code1, which might
+    # lead to unexpected behavior.
     for i, path_on_host in enumerate(unique_code_paths):
         mounted_code_path = f"{MEADOWRUN_CODE_MOUNT_LINUX}{i}"
         binds.append((path_on_host, mounted_code_path))

commit 4d0b32cc6d07ee555aaa2c8e976a5a6bfcf318ac

Fix a few issues in local_code.py introduced in be3d8bf: - Don't re-zip files if there are overlapping or duplicate python paths - Replace \ with / to make non-python files work on Windows - As a result, we can resurrect part of test_consolidate_paths that was deleted in that change (with modification)

src/meadowrun/local_code.py
@@ -75,6 +75,7 @@ def zip_local_code(
     (
         python_zip_paths,
         non_python_zip_paths,
+        real_python_root_paths_to_zip_paths,
     ) = _consolidate_paths_to_zip(real_python_paths_to_zip, non_python_paths_to_zip)
 
     # Currently we always zip all the files and then compute the hash. The hash is just
@@ -84,7 +85,7 @@ def zip_local_code(
     # building, so this is not currently a bottleneck.
     zip_file_path = join(result_zip_dir, str(uuid.uuid4()) + ".zip")
     with zipfile.ZipFile(zip_file_path, "w") as zip_file:
-        for real_path, zip_path in zip(real_python_paths_to_zip, python_zip_paths):
+        for real_path, zip_path in real_python_root_paths_to_zip_paths:
             for dirpath, _, filenames in os.walk(real_path):
                 for filename in filenames:
                     if splitext(filename)[1].lower() in python_paths_extensions:
@@ -95,9 +96,12 @@ def zip_local_code(
         for (file_real_path, dir_real_path), dir_zip_path in zip(
             real_file_paths_to_zip, itertools.islice(non_python_zip_paths, 1, None)
         ):
-            zip_file.write(
-                file_real_path, file_real_path.replace(dir_real_path, dir_zip_path, 1)
-            )
+            file_zip_path = file_real_path.replace(dir_real_path, dir_zip_path, 1)
+            if os.path.sep != "/":
+                # we have to make the sub-paths compatible with Linux which is our
+                # target OS
+                file_zip_path = file_zip_path.replace(os.path.sep, "/")
+            zip_file.write(file_real_path, file_zip_path)
 
     return zip_file_path, python_zip_paths, non_python_zip_paths[0]
 
@@ -123,9 +127,21 @@ def _get_python_paths_to_zip(
 
 def _consolidate_paths_to_zip(
     real_python_paths_to_zip: Iterable[str], real_non_python_paths_to_zip: Iterable[str]
-) -> Tuple[List[str], List[str]]:
-    """Generates a minimal dictionary of real paths that need to be zipped, to the paths
-    in the zip file.
+) -> Tuple[List[str], List[str], List[Tuple[str, str]]]:
+    """
+    Takes some real paths, and returns the path they should be in the zip. Some example
+    inputs/outputs of real paths to zip paths:
+    - [a/b/c, a/b] -> [b/c, b] (don't make c a separate directory because it's already
+      included in b
+    - [a/b, d/b] ->  [b, b_0] (we have two b folders that we need to keep separate)
+
+    Return values are python_zip_paths, non_python_zip_paths, python_roots
+
+    python_zip_paths and non_python_zip paths correspond 1-to-1 with the elements in
+    real_python_paths_to_zip and real_non_python_paths_to_zip.
+
+    python_roots is a subset of zip(real_python_paths_to_zip, python_zip_paths) that are
+    "roots". E.g. in the first example above, this will just return [(a/b, b)].
     """
 
     # This whole rigmarole because:
@@ -182,4 +198,21 @@ def _consolidate_paths_to_zip(
         real_paths_to_zip_paths[real_path] for real_path in real_non_python_paths_to_zip
     ]
 
-    return zip_python_paths, zip_non_python_paths
+    # consolidate python paths
+    python_root_candidates = tuple(sorted(real_python_paths_to_zip))
+    current_root = python_root_candidates[0]
+    real_python_root_paths = [current_root]
+    for candidate in python_root_candidates:
+        if not candidate.startswith(current_root):
+            # we have a new root.
+            current_root = candidate
+            real_python_root_paths.append(current_root)
+
+    return (
+        zip_python_paths,
+        zip_non_python_paths,
+        [
+            (real_path, real_paths_to_zip_paths[real_path])
+            for real_path in real_python_root_paths
+        ],
+    )

tests/test_local_code.py
@@ -7,23 +7,27 @@ from meadowrun import local_code
 
 
 @pytest.mark.parametrize(
-    "python_paths,other_paths,expected_python_paths,expected_other_paths",
+    "python_paths,other_paths,python_root_paths,expected_python_paths,"
+    "expected_other_paths",
     [
         (  # simplest case: one root with a few subfolders
             ["/usr/lib", "/usr/lib/python", "/usr/lib/python/more"],
             [],
+            {"/usr/lib": "lib"},
             ["lib", "lib/python", "lib/python/more"],
             [],
         ),
         (  # two roots each with a subfolder
             ["/usr/lib", "/usr/lib/python", "/home/foo/mdr", "/home/foo/mdr/src"],
             [],
+            {"/usr/lib": "lib", "/home/foo/mdr": "mdr"},
             ["lib", "lib/python", "mdr", "mdr/src"],
             [],
         ),
         (  # two roots without subfolders, but same name
             ["/home/user/lib2/src", "/home/user/lib1/src"],
             [],
+            {"/home/user/lib1/src": "src", "/home/user/lib2/src": "src_0"},
             ["src_0", "src"],
             [],
         ),
@@ -35,6 +39,7 @@ from meadowrun import local_code
                 "/home/user/lib2/src/bar",
             ],
             [],
+            {"/home/user/lib1/src": "src", "/home/user/lib2/src": "src_0"},
             ["src_0", "src", "src/foo", "src_0/bar"],
             [],
         ),
@@ -42,39 +47,49 @@ from meadowrun import local_code
         (  # simplest case: one root with a few subfolders
             ["/usr/lib/python", "/usr/lib/python/more"],
             ["/usr/lib"],
+            {"/usr/lib/python": "lib/python"},
             ["lib/python", "lib/python/more"],
             ["lib"],
         ),
         (  # two roots each with a subfolder
             ["/usr/lib", "/usr/lib/python"],
             ["/home/foo/mdr", "/home/foo/mdr/src"],
+            {"/usr/lib": "lib"},
             ["lib", "lib/python"],
             ["mdr", "mdr/src"],
         ),
         (  # two roots without subfolders, but same name
             ["/home/user/lib2/src"],
             ["/home/user/lib1/src"],
+            {"/home/user/lib2/src": "src_0"},
             ["src_0"],
             ["src"],
         ),
         (  # two roots with subfolders, same name
             ["/home/user/lib2/src", "/home/user/lib2/src/bar"],
             ["/home/user/lib1/src", "/home/user/lib1/src/foo"],
+            {"/home/user/lib2/src": "src_0"},
             ["src_0", "src_0/bar"],
             ["src", "src/foo"],
         ),
     ],
 )
 def test_consolidate_paths(
-    python_paths, other_paths, expected_python_paths, expected_other_paths
+    python_paths,
+    other_paths,
+    python_root_paths,
+    expected_python_paths,
+    expected_other_paths,
 ):
     (
         actual_python_paths,
         actual_other_paths,
+        actual_python_root_paths,
     ) = local_code._consolidate_paths_to_zip(python_paths, other_paths)
     assert len(actual_python_paths) == len(python_paths)
     assert len(actual_other_paths) == len(other_paths)
 
+    assert python_root_paths == dict(actual_python_root_paths)
     assert expected_python_paths == actual_python_paths
     assert expected_other_paths == actual_other_paths
 
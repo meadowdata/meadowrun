meadowdata/meadowrun

commit c13e34684929b5287998716dec7040f22ee657ed

Make Resources optional, as this is an option with Kubernetes. This also requires swapping the order of Host and Resources. And some minor changes: - Fix the type of the return value for Host.run_map - Rename Resources.to_resources to to_internal

src/meadowrun/run_job_core.py
@@ -90,9 +90,9 @@ class Resources:
         flags_required: E.g. "intel", "avx512", etc.
     """
 
-    logical_cpu: float
-    memory_gb: float
-    max_eviction_rate: float
+    logical_cpu: Optional[float] = None
+    memory_gb: Optional[float] = None
+    max_eviction_rate: float = 80
     gpus: Optional[float] = None
     gpu_memory: Optional[float] = None
     flags: Union[Iterable[str], str, None] = None
@@ -103,15 +103,18 @@ class Resources:
     def needs_cuda(self) -> bool:
         return self.uses_gpu() and _needs_cuda(self.flags)
 
-    def to_resources(self) -> ResourcesInternal:
-        return ResourcesInternal.from_cpu_and_memory(
-            self.logical_cpu,
-            self.memory_gb,
-            self.max_eviction_rate,
-            self.gpus,
-            self.gpu_memory,
-            self.flags,
-        )
+    def to_internal(self) -> Optional[ResourcesInternal]:
+        if self.logical_cpu is None or self.memory_gb is None:
+            return None
+        else:
+            return ResourcesInternal.from_cpu_and_memory(
+                self.logical_cpu,
+                self.memory_gb,
+                self.max_eviction_rate,
+                self.gpus,
+                self.gpu_memory,
+                self.flags,
+            )
 
 
 class Host(abc.ABC):
@@ -122,7 +125,7 @@ class Host(abc.ABC):
 
     @abc.abstractmethod
     async def run_job(
-        self, resources_required: ResourcesInternal, job: Job
+        self, resources_required: Optional[ResourcesInternal], job: Job
     ) -> JobCompletion[Any]:
         pass
 
@@ -131,11 +134,11 @@ class Host(abc.ABC):
         self,
         function: Callable[[_T], _U],
         args: Sequence[_T],
-        resources_required_per_task: ResourcesInternal,
+        resources_required_per_task: Optional[ResourcesInternal],
         job_fields: Dict[str, Any],
         num_concurrent_tasks: int,
         pickle_protocol: int,
-    ) -> Sequence[Any]:
+    ) -> Sequence[_U]:
         # Note for implementors: job_fields will be populated with everything other than
         # job_id and py_function, so the implementation should construct
         # Job(job_id=job_id, py_function=py_function, **job_fields)
@@ -159,8 +162,11 @@ class SshHost(Host):
     cloud_provider: Optional[Tuple[CloudProviderType, str]] = None
 
     async def run_job(
-        self, resources_required: ResourcesInternal, job: Job
+        self, resources_required: Optional[ResourcesInternal], job: Job
     ) -> JobCompletion[Any]:
+        if resources_required is not None:
+            raise ValueError("Specifying Resources for SshHost is not supported")
+
         # try the connection 20 times.
         connection = await _retry(
             lambda: ssh.connect(
@@ -281,11 +287,11 @@ class SshHost(Host):
         self,
         function: Callable[[_T], _U],
         args: Sequence[_T],
-        resources_required_per_task: ResourcesInternal,
+        resources_required_per_task: Optional[ResourcesInternal],
         job_fields: Dict[str, Any],
         num_concurrent_tasks: int,
         pickle_protocol: int,
-    ) -> Sequence[Any]:
+    ) -> Sequence[_U]:
         raise NotImplementedError("run_map is not implemented for SshHost")
 
 

src/meadowrun/kubernetes_integration.py
@@ -42,7 +42,8 @@ _U = TypeVar("_U")
 @dataclasses.dataclass(frozen=True)
 class Kubernetes(Host):
     """
-    Specifies a Kubernetes cluster to run a Meadowrun job on.
+    Specifies a Kubernetes cluster to run a Meadowrun job on. resources_required is
+    optional with the Kubernetes Host.
 
     Attributes:
         storage_bucket: Together, the storage_* arguments specify an S3-compatible
@@ -202,12 +203,16 @@ class Kubernetes(Host):
             raise ValueError(f"Unknown job_spec {job_spec_type}")
 
     async def run_job(
-        self, resources_required: ResourcesInternal, job: Job
+        self, resources_required: Optional[ResourcesInternal], job: Job
     ) -> JobCompletion[Any]:
         # This code encompasses everything that happens in SshHost.run_job and
         # run_job_local
 
-        # TODO take resources_required into account
+        # TODO add support for resources
+        if resources_required is not None:
+            raise NotImplementedError(
+                "Specifying Resources for a Kubernetes job is not yet supported"
+            )
 
         # detect any unsupported Jobs
 
@@ -377,11 +382,17 @@ class Kubernetes(Host):
         self,
         function: Callable[[_T], _U],
         args: Sequence[_T],
-        resources_required_per_task: ResourcesInternal,
+        resources_required_per_task: Optional[ResourcesInternal],
         job_fields: Dict[str, Any],
         num_concurrent_tasks: int,
         pickle_protocol: int,
-    ) -> Sequence[Any]:
+    ) -> Sequence[_U]:
+        # TODO add support for resources
+        if resources_required_per_task is not None:
+            raise NotImplementedError(
+                "Specifying Resources for a Kubernetes job is not yet supported"
+            )
+
         raise NotImplementedError("run_map is not implemented for Kubernetes yet")
 
 

src/meadowrun/run_job.py
@@ -918,7 +918,13 @@ async def _prepare_code_deployment(
 @dataclasses.dataclass(frozen=True)
 class AllocCloudInstance(Host):
     """
-    Specifies the requirements for a cloud instance (e.g. an EC2 instance or Azure VM).
+    Specifies that the job should be run on a dynamically allocated cloud instance (i.e.
+    either an EC2 instance or Azure VM) depending on the cloud_provider parameter. Any
+    existing Meadowrun-managed instances will be reused if available. If none are
+    available, Meadowrun will launch the cheapest instance that meets the resource
+    requirements for a job.
+
+    resources_required must be provided with the AllocCloudInstance Host.
 
     Attributes:
         cloud_provider: `EC2` or `AzureVM`
@@ -929,8 +935,14 @@ class AllocCloudInstance(Host):
     region_name: Optional[str] = None
 
     async def run_job(
-        self, resources_required: ResourcesInternal, job: Job
+        self, resources_required: Optional[ResourcesInternal], job: Job
     ) -> JobCompletion[Any]:
+        if resources_required is None:
+            raise ValueError(
+                "Resources.logical_cpu and memory_gb must be specified for "
+                "AllocCloudInstance"
+            )
+
         if self.cloud_provider == "EC2":
             return await run_job_ec2_instance_registrar(
                 job,
@@ -950,11 +962,17 @@ class AllocCloudInstance(Host):
         self,
         function: Callable[[_T], _U],
         args: Sequence[_T],
-        resources_required_per_task: ResourcesInternal,
+        resources_required_per_task: Optional[ResourcesInternal],
         job_fields: Dict[str, Any],
         num_concurrent_tasks: int,
         pickle_protocol: int,
-    ) -> Sequence[Any]:
+    ) -> Sequence[_U]:
+        if resources_required_per_task is None:
+            raise ValueError(
+                "Resources.logical_cpu and memory_gb must be specified for "
+                "AllocCloudInstance"
+            )
+
         if self.cloud_provider == "EC2":
             helper = await prepare_ec2_run_map(
                 function,
@@ -1128,8 +1146,8 @@ def _prepare_container_services(
 
 async def run_function(
     function: Union[Callable[..., _T], str],
-    resources_required: Resources,
     host: Host,
+    resources_required: Optional[Resources] = None,
     deployment: Optional[Deployment] = None,
     args: Optional[Sequence[Any]] = None,
     kwargs: Optional[Dict[str, Any]] = None,
@@ -1146,10 +1164,11 @@ async def run_function(
             lambda, or a string like `"package.module.function_name"` (which is useful
             if the function cannot be referenced in the current environment but can be
             referenced in the deployed environment)
-        resources_required: Specifies the resources (e.g. CPU, RAM) needed by the
-            function. See [Resources][meadowrun.Resources].
         host: Specifies where to run the function. See
             [AllocCloudInstance][meadowrun.AllocCloudInstance].
+        resources_required: Specifies the resources (e.g. CPU, RAM) needed by the
+            function. For some hosts, this is optional, for other hosts it is required.
+            See [Resources][meadowrun.Resources].
         deployment: See [Deployment][meadowrun.Deployment]. Specifies the
             environment (code and libraries) that are needed to run this function
         args: Passed to the function like `function(*args)`
@@ -1164,6 +1183,9 @@ async def run_function(
     Returns:
         The result of calling `function`
     """
+
+    if resources_required is None:
+        resources_required = Resources()
 
     pickle_protocol = _pickle_protocol_for_deployed_interpreter()
 
@@ -1247,14 +1269,14 @@ async def run_function(
         },
     )
 
-    job_completion = await host.run_job(resources_required.to_resources(), job)
+    job_completion = await host.run_job(resources_required.to_internal(), job)
     return job_completion.result
 
 
 async def run_command(
     args: Union[str, Sequence[str]],
-    resources_required: Resources,
     host: Host,
+    resources_required: Optional[Resources] = None,
     deployment: Optional[Deployment] = None,
     context_variables: Optional[Dict[str, Any]] = None,
     container_services: Union[
@@ -1269,10 +1291,11 @@ async def run_command(
         args: Specifies the command to run, can be a string (e.g. `"jupyter nbconvert
             --to html analysis.ipynb"`) or a list of strings (e.g. `["jupyter",
             --"nbconvert", "--to", "html", "analysis.ipynb"]`)
-        resources_required: Specifies the resources (e.g. CPU, RAM) needed by the
-            command. See [Resources][meadowrun.Resources].
         host: Specifies where to run the function. See
             [AllocCloudInstance][meadowrun.AllocCloudInstance].
+        resources_required: Specifies the resources (e.g. CPU, RAM) needed by the
+            command. For some hosts, this is optional, for other hosts it is required.
+            See [Resources][meadowrun.Resources].
         deployment: See [Deployment][meadowrun.Deployment]. Specifies
             the environment (code and libraries) that are needed to run this command
         context_variables: Experimental feature
@@ -1286,6 +1309,9 @@ async def run_command(
     Returns:
         A JobCompletion object that contains metadata about the running of the job.
     """
+
+    if resources_required is None:
+        resources_required = Resources()
 
     job_id = str(uuid.uuid4())
     if isinstance(args, str):
@@ -1341,14 +1367,14 @@ async def run_command(
         },
     )
 
-    return await host.run_job(resources_required.to_resources(), job)
+    return await host.run_job(resources_required.to_internal(), job)
 
 
 async def run_map(
     function: Callable[[_T], _U],
     args: Sequence[_T],
-    resources_required_per_task: Resources,
     host: Host,
+    resources_required_per_task: Optional[Resources],
     deployment: Optional[Deployment] = None,
     num_concurrent_tasks: Optional[int] = None,
     container_services: Union[
@@ -1365,7 +1391,8 @@ async def run_map(
         args: A list of objects, each item in the list represents a "task",
             where each "task" is an invocation of `function` on the item in the list
         resources_required_per_task: The resources (e.g. CPU and RAM) required to run a
-            single task. See [Resources][meadowrun.Resources].
+            single task. For some hosts, this is optional, for other hosts it is
+            required. See [Resources][meadowrun.Resources].
         host: Specifies where to get compute resources from. See
             [AllocCloudInstance][meadowrun.AllocCloudInstance].
         num_concurrent_tasks: The number of workers to launch. This can be less than or
@@ -1383,6 +1410,9 @@ async def run_map(
     Returns:
         Returns the result of running `function` on each of `args`
     """
+
+    if resources_required_per_task is None:
+        resources_required_per_task = Resources()
 
     if not num_concurrent_tasks:
         num_concurrent_tasks = len(args) // 2 + 1
@@ -1432,7 +1462,7 @@ async def run_map(
     return await host.run_map(
         function,
         args,
-        resources_required_per_task.to_resources(),
+        resources_required_per_task.to_internal(),
         job_fields,
         num_concurrent_tasks,
         pickle_protocol,

src/meadowrun/run_job_local.py
@@ -1146,8 +1146,11 @@ async def run_local(
 @dataclasses.dataclass(frozen=True)
 class LocalHost(Host):
     async def run_job(
-        self, resources_required: ResourcesInternal, job: Job
+        self, resources_required: Optional[ResourcesInternal], job: Job
     ) -> JobCompletion[Any]:
+        if resources_required is not None:
+            raise ValueError("Specifying Resources for LocalHost is not supported")
+
         initial_update, continuation = await run_local(job)
         if (
             initial_update.state != ProcessState.ProcessStateEnum.RUNNING
@@ -1180,9 +1183,9 @@ class LocalHost(Host):
         self,
         function: Callable[[_T], _U],
         args: Sequence[_T],
-        resources_required_per_task: ResourcesInternal,
+        resources_required_per_task: Optional[ResourcesInternal],
         job_fields: Dict[str, Any],
         num_concurrent_tasks: int,
         pickle_protocol: int,
-    ) -> Sequence[Any]:
+    ) -> Sequence[_U]:
         raise NotImplementedError("run_map on LocalHost is not implemented")

tests/automated/test_local_automated.py
@@ -90,16 +90,16 @@ class TestBasicsLocal(LocalHostProvider, BasicsSuite):
         """
         job_completion1 = await run_command(
             "python example_script.py",
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment(
                 code=ServerAvailableFolder(code_paths=[EXAMPLE_CODE, MEADOWRUN_CODE])
             ),
         )
         job_completion2 = await run_command(
             "python example_script.py",
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment(
                 code=ServerAvailableFolder(code_paths=[EXAMPLE_CODE, MEADOWRUN_CODE])
             ),

tests/basics.py
@@ -121,8 +121,8 @@ class BasicsSuite(HostProvider, abc.ABC):
     ):
         results: str = await run_function(
             "example_package.example.example_runner",
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment(interpreter_deployment, code_deployment),
             args=["foo"],
         )
@@ -130,8 +130,8 @@ class BasicsSuite(HostProvider, abc.ABC):
 
         job_completion = await run_command(
             "pip --version",
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment(interpreter_deployment, code_deployment),
         )
 
@@ -143,8 +143,8 @@ class BasicsSuite(HostProvider, abc.ABC):
 
         results: str = await run_function(
             "example.example_runner",
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment(
                 code=GitRepoCommit(
                     repo_url=self.get_test_repo_url(),
@@ -171,8 +171,8 @@ class BasicsSuite(HostProvider, abc.ABC):
 
             result = await run_command(
                 "python --version",
-                self.get_resources_required(),
                 self.get_host(),
+                self.get_resources_required(),
                 Deployment(ContainerAtDigest(repository="python", digest=digest)),
             )
 
@@ -190,8 +190,8 @@ class BasicsSuite(HostProvider, abc.ABC):
     async def test_conda_file_in_git_repo(self):
         results = await run_function(
             self._get_remote_function_for_deployment(),
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment.git_repo(
                 repo_url=self.get_test_repo_url(),
                 branch="main",
@@ -206,8 +206,8 @@ class BasicsSuite(HostProvider, abc.ABC):
     async def test_pip_file_in_git_repo(self):
         results = await run_function(
             self._get_remote_function_for_deployment(),
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment.git_repo(
                 repo_url=self.get_test_repo_url(),
                 branch="main",
@@ -222,8 +222,8 @@ class BasicsSuite(HostProvider, abc.ABC):
     async def test_pip_file_in_git_repo_with_git_dependency(self):
         results = await run_function(
             self._get_remote_function_for_deployment(),
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment.git_repo(
                 repo_url=self.get_test_repo_url(),
                 branch="main",
@@ -246,8 +246,8 @@ class BasicsSuite(HostProvider, abc.ABC):
 
         results = await run_function(
             remote_function,
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment.git_repo(
                 repo_url=self.get_test_repo_url(),
                 path_to_source="example_package",
@@ -268,8 +268,8 @@ class BasicsSuite(HostProvider, abc.ABC):
 
         results = await run_function(
             remote_function,
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment.git_repo(
                 repo_url=self.get_test_repo_url(),
                 path_to_source="example_package",
@@ -289,8 +289,8 @@ class BasicsSuite(HostProvider, abc.ABC):
 
         results = await run_function(
             remote_function,
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment.git_repo(
                 repo_url=self.get_test_repo_url(),
                 interpreter=PipRequirementsFile("requirements.txt", "3.9"),
@@ -303,8 +303,8 @@ class BasicsSuite(HostProvider, abc.ABC):
     async def test_poetry_project_in_git_repo(self):
         results = await run_function(
             self._get_remote_function_for_deployment(),
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment.git_repo(
                 repo_url=self.get_test_repo_url(),
                 branch="main",
@@ -319,8 +319,8 @@ class BasicsSuite(HostProvider, abc.ABC):
     async def test_poetry_project_in_git_repo_with_git_dependency(self):
         results = await run_function(
             self._get_remote_function_for_deployment(),
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment.git_repo(
                 repo_url=self.get_test_repo_url(),
                 branch="main",
@@ -338,8 +338,8 @@ class BasicsSuite(HostProvider, abc.ABC):
     async def test_git_repo_with_container(self):
         results = await run_function(
             self._get_remote_function_for_deployment(),
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             Deployment.git_repo(
                 repo_url=self.get_test_repo_url(),
                 branch="main",
@@ -358,8 +358,8 @@ class BasicsSuite(HostProvider, abc.ABC):
         try:
             results = await run_function(
                 self._get_remote_function_for_deployment(),
-                self.get_resources_required(),
                 self.get_host(),
+                self.get_resources_required(),
                 await Deployment.mirror_local(
                     interpreter=LocalCondaInterpreter("test_repo_conda_env"),
                     additional_python_paths=[
@@ -397,8 +397,8 @@ class BasicsSuite(HostProvider, abc.ABC):
             test_venv_interpreter = _path_from_here("../../test_venv_linux/bin/python")
         results = await run_function(
             self._get_remote_function_for_deployment(),
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             await Deployment.mirror_local(
                 interpreter=LocalPipInterpreter(test_venv_interpreter, "3.9"),
                 additional_python_paths=[
@@ -418,8 +418,8 @@ class BasicsSuite(HostProvider, abc.ABC):
         # - pip install -r test_repo/requirements.txt
         results = await run_function(
             self._get_remote_function_for_deployment(),
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             await Deployment.mirror_local(
                 interpreter=CondaEnvironmentYmlFile(
                     _path_from_here("../../test_repo/myenv.yml")
@@ -441,8 +441,8 @@ class BasicsSuite(HostProvider, abc.ABC):
         # - pip install -r test_repo/requirements.txt
         results = await run_function(
             self._get_remote_function_for_deployment(),
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             await Deployment.mirror_local(
                 interpreter=PipRequirementsFile(
                     _path_from_here("../../test_repo/requirements.txt"), "3.9"
@@ -470,8 +470,8 @@ class BasicsSuite(HostProvider, abc.ABC):
 
             results = await run_function(
                 remote_function,
-                self.get_resources_required(),
                 self.get_host(),
+                self.get_resources_required(),
                 await Deployment.mirror_local(
                     interpreter=PipRequirementsFile(
                         _path_from_here("../../test_repo/requirements.txt"), "3.9"
@@ -488,8 +488,8 @@ class BasicsSuite(HostProvider, abc.ABC):
     async def test_local_poetry_project(self):
         results = await run_function(
             self._get_remote_function_for_deployment(),
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             await Deployment.mirror_local(
                 interpreter=PoetryProjectPath(
                     _path_from_here("../../test_repo/"), "3.9"
@@ -506,8 +506,8 @@ class BasicsSuite(HostProvider, abc.ABC):
     async def test_local_code_with_container(self):
         results = await run_function(
             self._get_remote_function_for_deployment(),
-            self.get_resources_required(),
             self.get_host(),
+            self.get_resources_required(),
             await Deployment.mirror_local(
                 interpreter=ContainerInterpreter("hrichardlee/meadowrun_test_env"),
                 additional_python_paths=[
@@ -541,8 +541,8 @@ class ErrorsSuite(HostProvider, abc.ABC):
         with pytest.raises(MeadowrunException) as exc_info:
             await run_function(
                 lambda: "hello",
-                self.get_resources_required(),
                 self.get_host(),
+                self.get_resources_required(),
                 Deployment(ServerAvailableContainer(image_name="does-not-exist")),
             )
 
@@ -559,7 +559,7 @@ class ErrorsSuite(HostProvider, abc.ABC):
 
         with pytest.raises(MeadowrunException) as exc_info:
             await run_function(
-                exit_immediately, self.get_resources_required(), self.get_host()
+                exit_immediately, self.get_host(), self.get_resources_required()
             )
 
         assert (
@@ -581,8 +581,8 @@ class MapSuite(abc.ABC):
         results = await run_map(
             lambda x: x**x,
             [1, 2, 3, 4],
-            Resources(1, 1, 15),
             AllocCloudInstance(self.cloud_provider()),
+            Resources(1, 1, 15),
             num_concurrent_tasks=3,
         )
 

tests/benchmarks/ec2_startup_time.py
@@ -30,8 +30,8 @@ async def main():
             start_time = monotonic()
             pid1, host1 = await run_function(
                 remote_function,
-                Resources(1, 0.5, 80),
                 AllocCloudInstance(irp.cloud_provider()),
+                Resources(1, 0.5, 80),
             )
             times.append(monotonic() - start_time)
     print("Times in secs:")

tests/instance_registrar_suite.py
@@ -265,20 +265,20 @@ class InstanceRegistrarSuite(InstanceRegistrarProvider, abc.ABC):
 
             pid1, host1 = await run_function(
                 remote_function,
-                Resources(1, 0.5, 15),
                 AllocCloudInstance(self.cloud_provider()),
+                Resources(1, 0.5, 15),
             )
             time.sleep(1)
             pid2, host2 = await run_function(
                 remote_function,
-                Resources(1, 0.5, 15),
                 AllocCloudInstance(self.cloud_provider()),
+                Resources(1, 0.5, 15),
             )
             time.sleep(1)
             pid3, host3 = await run_function(
                 remote_function,
-                Resources(1, 0.5, 15),
                 AllocCloudInstance(self.cloud_provider()),
+                Resources(1, 0.5, 15),
             )
 
             # these should have all run on the same host, but in different processes
@@ -305,22 +305,22 @@ class InstanceRegistrarSuite(InstanceRegistrarProvider, abc.ABC):
             task1 = asyncio.create_task(
                 run_function(
                     remote_function,
-                    Resources(1, 0.5, 15),
                     AllocCloudInstance(self.cloud_provider()),
+                    Resources(1, 0.5, 15),
                 )
             )
             task2 = asyncio.create_task(
                 run_function(
                     remote_function,
-                    Resources(1, 0.5, 15),
                     AllocCloudInstance(self.cloud_provider()),
+                    Resources(1, 0.5, 15),
                 )
             )
             task3 = asyncio.create_task(
                 run_function(
                     remote_function,
-                    Resources(1, 0.5, 15),
                     AllocCloudInstance(self.cloud_provider()),
+                    Resources(1, 0.5, 15),
                 )
             )
 

commit 68678a28dce314310baf4896744466d26ffd25f0

Fixing a bug introduced in c13e3468, we pass resources_required through to SshHost for AllocCloudInstance, so we should not throw when it is None
Also fix flake8 errors not caught in the last commit

build_scripts/build_ami_helper.py
@@ -98,8 +98,8 @@ async def _build_ami(
 
     plain_base_image_actions_on_vm must return the ami name
 
-    You must call _wait_for_image_and_make_public(region_name, image_id) after this function, and then
-    terminate the instance_id
+    You must call _wait_for_image_and_make_public(region_name, image_id) after this
+    function, and then terminate the instance_id
 
     Returns success, region_name (same as function argument), image id if success,
     otherwise exception traceback, instance_id
@@ -187,9 +187,7 @@ async def build_amis(
     actions_on_vm: Callable[[asyncssh.SSHClientConnection], Awaitable[str]],
 ):
     """Builds the same AMI in every region specified by regions"""
-    region_to_base_amis = [
-        (region, all_region_base_amis[region]) for region in regions
-    ]
+    region_to_base_amis = [(region, all_region_base_amis[region]) for region in regions]
 
     print(
         "Will build image in " + ", ".join(region for region, _ in region_to_base_amis)

build_scripts/build_base_ami.py
@@ -175,10 +175,10 @@ async def parse_cuda_version(connection: asyncssh.SSHClientConnection) -> str:
 
 def main():
     r"""
-    A script for creating a meadowrun EC2 base AMIs. The only reason to want to change the
-    base AMI is because you want to change the version or flavour of Linux/Docker/Python
-    we're using, or some other deep baked-into-the-OS thing. To update meadowrun, you can
-    just run build_ami.py
+    A script for creating a meadowrun EC2 base AMIs. The only reason to want to change
+    the base AMI is because you want to change the version or flavour of
+    Linux/Docker/Python we're using, or some other deep baked-into-the-OS thing. To
+    update meadowrun, you can just run build_ami.py
 
     plain images are the "regular" base images. cuda images have cuda installed.
 

src/meadowrun/run_job_core.py
@@ -165,9 +165,6 @@ class SshHost(Host):
     async def run_job(
         self, resources_required: Optional[ResourcesInternal], job: Job
     ) -> JobCompletion[Any]:
-        if resources_required is not None:
-            raise ValueError("Specifying Resources for SshHost is not supported")
-
         # try the connection 20 times.
         connection = await _retry(
             lambda: ssh.connect(
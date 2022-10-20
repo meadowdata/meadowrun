766267a2bc457b007bec98871ee0dd33007e7f09
Hyunho Richard Lee <hrichardlee@gmail.com>
2022-10-20 18:31:47 -0400

    Have build-dev.bat push to docker (for working with GKE)

--- docker_images/meadowrun/build-dev.bat
+++ docker_images/meadowrun/build-dev.bat
@@ -8,14 +8,18 @@ echo Meadowrun version: %MEADOWRUN_VERSION%
 
 call poetry build
 
-@REM docker build -t meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.10 --build-arg PYTHON_VERSION=3.10 --build-arg MEADOWRUN_VERSION=%MEADOWRUN_VERSION% -f docker_images/meadowrun/Dockerfile dist
+docker build -t meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.10 --build-arg PYTHON_VERSION=3.10 --build-arg MEADOWRUN_VERSION=%MEADOWRUN_VERSION% -f docker_images/meadowrun/Dockerfile dist
 @REM minikube image load meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.10
+docker push meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.10
 
 docker build -t meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.9 --build-arg PYTHON_VERSION=3.9 --build-arg MEADOWRUN_VERSION=%MEADOWRUN_VERSION% -f docker_images/meadowrun/Dockerfile dist
-minikube image load meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.9
+@REM minikube image load meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.9
+docker push meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.9
 
 @REM docker build -t meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.8 --build-arg PYTHON_VERSION=3.8 --build-arg MEADOWRUN_VERSION=%MEADOWRUN_VERSION% -f docker_images/meadowrun/Dockerfile dist
 @REM minikube image load meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.8
+@REM docker push meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.8
 
 @REM docker build -t meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.7 --build-arg PYTHON_VERSION=3.7 --build-arg MEADOWRUN_VERSION=%MEADOWRUN_VERSION% -f docker_images/meadowrun/Dockerfile dist
 @REM minikube image load meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.7
+@REM docker push meadowrun/meadowrun:%MEADOWRUN_VERSION%-py3.7

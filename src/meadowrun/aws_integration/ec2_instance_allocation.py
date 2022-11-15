meadowdata/meadowrun

commit b896443d305b5b859a5f292914894fb0aa0999a3

For EC2, use SQS to launch workers rather than SSH. This significantly improves performance when starting many workers (we could have gotten similar performance improvements by just starting multiple workers at once over a single SSH session, but this is likely faster when there are many machines involved. Also, this makes configuration easier--machines always need access to SQS for run_map jobs, but being open to SSH connections is optional).

src/meadowrun/aws_integration/ec2_instance_allocation.py
@@ -5,6 +5,7 @@ import dataclasses
 import datetime
 import decimal
 import itertools
+import json
 from typing import (
     TYPE_CHECKING,
     Any,
@@ -75,6 +76,7 @@ from meadowrun.run_job_core import (
 from meadowrun.alloc_vm import (
     AllocVM,
     GridJobCloudInterface,
+    GridJobQueueWorkerLauncher,
     GridJobSshWorkerLauncher,
     GridJobWorkerLauncher,
 )
@@ -84,6 +86,7 @@ from meadowrun.storage_grid_job import (
     receive_results,
 )
 from meadowrun.meadowrun_pb2 import QualifiedFunctionName
+from meadowrun.storage_keys import storage_key_job_to_run
 
 if TYPE_CHECKING:
     from types import TracebackType
@@ -104,7 +107,7 @@ _U = TypeVar("_U")
 # replicate into each region.
 _AMIS = {
     "plain": {
-        "us-east-2": "ami-0667e31e8579f4b37",
+        "us-east-2": "ami-0225b27022c2aff4c",
         "us-east-1": "ami-06f3ad92d9b203ac4",
         "us-west-1": "ami-0634aeae222fc2b17",
         "us-west-2": "ami-0e05dba977392eccb",
@@ -694,7 +697,7 @@ class AllocEC2Instance(AllocVM):
         job_fields: Dict[str, Any],
         wait_for_result: WaitOption,
     ) -> GridJobWorkerLauncher:
-        return EC2GridJobSshWorkerLauncher(
+        return EC2GridJobQueueWorkerLauncher(
             self,
             base_job_id,
             user_function,
@@ -726,6 +729,7 @@ async def run_job_ec2_instance_registrar(
             job.ports,
         )
 
+    # TODO consider switching this to use the SQS route?
     return await SshHost(host, SSH_USER, pkey, ("EC2", region_name)).run_cloud_job(
         job, job_id, wait_for_result, None
     )
@@ -914,3 +918,91 @@ class EC2GridJobSshWorkerLauncher(GridJobSshWorkerLauncher):
             (self._cloud_provider, self._region_name),
             instance_name,
         )
+
+
+MACHINE_AGENT_QUEUE_PREFIX = "meadowrun-machine-"
+
+
+class EC2GridJobQueueWorkerLauncher(GridJobQueueWorkerLauncher):
+    def __init__(
+        self,
+        alloc_ec2_instance: AllocEC2Instance,
+        base_job_id: str,
+        user_function: Callable[[_T], _U],
+        pickle_protocol: int,
+        job_fields: Dict[str, Any],
+        wait_for_result: WaitOption,
+    ):
+        self._region_name = alloc_ec2_instance._get_region_name()
+        self._alloc_ec2_instance = alloc_ec2_instance
+
+        # this calls create_instance_registrar so it has to happen after
+        # self._region_name is set
+        super().__init__(
+            alloc_ec2_instance,
+            base_job_id,
+            user_function,
+            pickle_protocol,
+            job_fields,
+            wait_for_result,
+        )
+
+    def create_instance_registrar(self) -> InstanceRegistrar:
+        return EC2InstanceRegistrar(self._region_name, "create")
+
+    async def _upload_job_object(self, job_object_id: str, job: Job) -> None:
+        async with await self._alloc_ec2_instance.get_storage_bucket() as storage_bucket:  # noqa: E501
+            await storage_bucket.write_bytes(
+                job.SerializeToString(), storage_key_job_to_run(job_object_id)
+            )
+
+    async def launch_jobs(
+        self,
+        public_address: str,
+        instance_name: str,
+        job_object_id: str,
+        job_ids: List[str],
+    ) -> None:
+        queue_name = f"{MACHINE_AGENT_QUEUE_PREFIX}{instance_name}"
+
+        session = aiobotocore.session.get_session()
+        async with session.create_client(
+            "sqs", region_name=self._region_name
+        ) as sqs_client:
+            # not ideal that we create the queue every time but it's not any faster to
+            # query and see if the queue exists
+            queue_url = (
+                await sqs_client.create_queue(
+                    QueueName=queue_name, tags={_MEADOWRUN_TAG: _MEADOWRUN_TAG_VALUE}
+                )
+            )["QueueUrl"]
+            result = await sqs_client.send_message(
+                QueueUrl=queue_url,
+                MessageBody=json.dumps(
+                    {
+                        "action": "launch",
+                        "job_object_id": job_object_id,
+                        "job_ids": job_ids,
+                        "public_address": public_address,
+                    }
+                ),
+            )
+            if "Failed" in result:
+                raise ValueError(f"Unable to add jobs to the queue: {result}")
+
+    async def kill_jobs(self, instance_name: str, job_ids: List[str]) -> None:
+        queue_name = f"{MACHINE_AGENT_QUEUE_PREFIX}{instance_name}"
+
+        session = aiobotocore.session.get_session()
+        async with session.create_client(
+            "sqs", region_name=self._region_name
+        ) as sqs_client:
+            queue_url = (await sqs_client.get_queue_url(QueueName=queue_name))[
+                "QueueUrl"
+            ]
+            result = await sqs_client.send_message(
+                QueueUrl=queue_url,
+                MessageBody=json.dumps({"action": "kill", "job_ids": job_ids}),
+            )
+            if "Failed" in result:
+                raise ValueError(f"Unable to add kill messages to the queue: {result}")

src/meadowrun/aws_integration/machine_agent.py
@@ -1,0 +1,288 @@
+import asyncio
+import dataclasses
+import json
+import os
+import signal
+import subprocess
+import sys
+import time
+import traceback
+from typing import Optional, Dict, Any
+
+import aiobotocore.session
+
+from meadowrun.aws_integration.aws_core import _get_ec2_metadata
+from meadowrun.aws_integration.ec2_instance_allocation import (
+    EC2InstanceRegistrar,
+    MACHINE_AGENT_QUEUE_PREFIX,
+)
+from meadowrun.aws_integration.management_lambdas.ec2_alloc_stub import (
+    _MEADOWRUN_TAG,
+    _MEADOWRUN_TAG_VALUE,
+)
+from meadowrun.run_job_core import get_log_path
+from meadowrun.storage_grid_job import get_aws_s3_bucket
+from meadowrun.storage_keys import storage_key_job_to_run
+
+
+@dataclasses.dataclass
+class ProcessWrapper:
+    process: Optional[subprocess.Popen]
+    killed: bool
+    last_updated: float
+
+    def get_status(self) -> str:
+        if self.process is None:
+            if not self.killed:
+                return "unexpected status: process is None and killed = True"
+            else:
+                return "kill was requested before process was launched"
+        else:
+            return_code = self.process.poll()
+            if return_code is None:
+                return f"{self.process.pid} is running"
+            else:
+                return f"{self.process.pid} exited with return code {return_code}"
+
+
+@dataclasses.dataclass
+class CachedJobObject:
+    job_id: str
+    last_used: float
+
+
+async def main() -> None:
+    """
+    There should be one of these processes running per EC2 instance. This will listen to
+    the machine-specific SQS queue for messages that tell it to launch/kill jobs on this
+    instance.
+    """
+    instance_id = await _get_ec2_metadata("instance-id")
+    if not instance_id:
+        raise ValueError("Cannot get instance-id from IMDS endpoint")
+    region_name = await _get_ec2_metadata("placement/region")
+    if not region_name:
+        raise ValueError("Cannot get placement/region from IMDS endpoint")
+
+    print(f"Running with instance id {instance_id} in {region_name}")
+
+    environment = os.environ.copy()
+    # shouldn't be needed if the process is launched correctly
+    environment["PYTHONUNBUFFERED"] = "1"
+
+    session = aiobotocore.session.get_session()
+    async with session.create_client(
+        "sqs", region_name=region_name
+    ) as sqs_client, EC2InstanceRegistrar(region_name, "raise") as instance_registrar:
+        queue_url = (
+            await sqs_client.create_queue(
+                QueueName=f"{MACHINE_AGENT_QUEUE_PREFIX}{instance_id}",
+                tags={_MEADOWRUN_TAG: _MEADOWRUN_TAG_VALUE},
+            )
+        )["QueueUrl"]
+
+        print(f"Listening to queue {queue_url}")
+
+        # maps from (job_id_override OR job_id) -> ProcessWrapper
+        processes: Dict[str, ProcessWrapper] = {}
+        job_objects: Dict[str, CachedJobObject] = {}
+
+        while True:
+            try:
+                messages_result = await sqs_client.receive_message(
+                    QueueUrl=queue_url, WaitTimeSeconds=20, MaxNumberOfMessages=10
+                )
+                if "Messages" not in messages_result:
+                    await _clean_up(processes, job_objects)
+                    continue
+
+                messages = messages_result["Messages"]
+            except (asyncio.CancelledError, KeyboardInterrupt):
+                raise
+            except BaseException:
+                print("Error receiving messages:\n" + traceback.format_exc())
+                continue
+
+            for message in messages:
+                message_content = None
+                try:
+                    message_content = json.loads(message["Body"])
+                    action = message_content.get("action", None)
+
+                    if action == "launch":
+                        await _launch_jobs(
+                            message_content,
+                            processes,
+                            job_objects,
+                            region_name,
+                            environment,
+                        )
+                    elif action == "kill":
+                        await _kill_jobs(
+                            message_content, instance_id, instance_registrar, processes
+                        )
+                    else:
+                        print(
+                            f"Unexpected action {action} in message {message_content}"
+                        )
+                except (asyncio.CancelledError, KeyboardInterrupt):
+                    raise
+                except BaseException:
+                    print(
+                        f"Error acting on message {message_content}:\n"
+                        + traceback.format_exc()
+                    )
+                finally:
+                    # TODO it would be nice to retry instead of just giving up after one
+                    # failure
+                    if message_content is not None and "ReceiptHandle" in message:
+                        await sqs_client.delete_message(
+                            QueueUrl=queue_url, ReceiptHandle=message["ReceiptHandle"]
+                        )
+
+
+def _local_job_to_run_file(job_object_id: str) -> str:
+    return f"/var/meadowrun/io/{job_object_id}.job_to_run"
+
+
+async def _launch_jobs(
+    message_content: Dict[str, Any],
+    processes: Dict[str, ProcessWrapper],
+    job_objects: Dict[str, CachedJobObject],
+    region_name: str,
+    environment: Dict[str, str],
+) -> None:
+    """Modifies processes in place!"""
+
+    t0 = time.time()
+
+    if "job_object_id" not in message_content or "job_ids" not in message_content:
+        print(f"Warning: ignoring incomplete launch message: {message_content}")
+        return
+
+    job_object_id = message_content["job_object_id"]
+    if job_object_id not in job_objects:
+        async with get_aws_s3_bucket(region_name) as storage_bucket:
+            await storage_bucket.get_file(
+                storage_key_job_to_run(job_object_id),
+                _local_job_to_run_file(job_object_id),
+            )
+        job_objects[job_object_id] = CachedJobObject(job_object_id, t0)
+    else:
+        job_objects[job_object_id].last_used = t0
+
+    if "public_address" in message_content:
+        public_address = f"--public-address {message_content['public_address']}"
+    else:
+        public_address = ""
+
+    for job_id in message_content["job_ids"]:
+        if job_id not in processes:
+            if sys.platform != "win32":
+                process = subprocess.Popen(
+                    "/var/meadowrun/env/bin/python -m meadowrun.run_job_local_main "
+                    f"--job-id {job_id} --cloud EC2 --cloud-region-name {region_name} "
+                    f"--job-object-id {job_object_id} {public_address} "
+                    f"> {get_log_path(job_id)} 2>&1",
+                    shell=True,
+                    env=environment,
+                    # see https://stackoverflow.com/a/4791612/908704
+                    preexec_fn=os.setsid,
+                )
+                processes[job_id] = ProcessWrapper(process, False, t0)
+                print(f"Started job {job_id}")
+            else:
+                # just for mypy
+                raise ValueError("os.setsid not available on Windows")
+        else:
+            print(
+                f"Unexpected: duplicate request to run {job_id}: "
+                f"{processes[job_id].get_status()}"
+            )
+
+
+async def _kill_jobs(
+    message_content: Dict[str, Any],
+    instance_id: str,
+    instance_registrar: EC2InstanceRegistrar,
+    processes: Dict[str, ProcessWrapper],
+) -> None:
+
+    t0 = time.time()
+
+    if "job_ids" not in message_content:
+        print(f"Warning: ignoring incomplete kill message {message_content}")
+        return
+
+    registered_instance = await instance_registrar.get_registered_instance(instance_id)
+
+    for job_id in message_content["job_ids"]:
+        if job_id in processes:
+            process_wrapper = processes[job_id]
+            process = process_wrapper.process
+            if process is not None:
+                if process.poll() is None:  # i.e. process is still running
+                    if sys.platform != "win32":
+                        os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+                    else:
+                        # this is just for mypy
+                        raise ValueError(
+                            "os.killpg and os.getpgid not implemented on Windows"
+                        )
+
+                    if job_id in registered_instance.get_running_jobs():
+                        await instance_registrar.deallocate_job_from_instance(
+                            registered_instance, job_id
+                        )
+
+                    print(f"Killed and deallocated job {job_id}")
+
+                    process_wrapper.last_updated = t0
+                    process_wrapper.killed = True
+        else:
+            processes[job_id] = ProcessWrapper(None, True, t0)
+            print(f"Unexpected: requested to kill a job that is not running {job_id}")
+
+
+async def _clean_up(
+    processes: Dict[str, ProcessWrapper], job_objects: Dict[str, CachedJobObject]
+) -> None:
+    """
+    Modifies processes in place, removes the ones that have exited (or never started)
+    more than 120 seconds ago.
+
+    Modifies job_objects in place, deletes the file and entry for objects that haven't
+    been used in the last 30 minutes.
+    """
+    t0 = time.time()
+    old_job_ids = []
+    for job_id, process_wrapper in processes.items():
+        # we need to poll even if we don't care about the return code of the child
+        # process otherwise the child processes will become zombies
+        if (
+            process_wrapper.process is None
+            or process_wrapper.process.poll() is not None  # i.e. process has exited
+        ) and process_wrapper.last_updated < t0 - 120:
+            old_job_ids.append(job_id)
+    for job_id in old_job_ids:
+        del processes[job_id]
+
+    old_job_object_ids = []
+    for job_object_id, job_object in job_objects.items():
+        if job_object.last_used < t0 - 60 * 30:
+            try:
+                os.remove(_local_job_to_run_file(job_object_id))
+                old_job_object_ids.append(job_object_id)
+            except asyncio.CancelledError:
+                raise
+            except BaseException:
+                print(
+                    f"Warning, error deleting {_local_job_to_run_file(job_object_id)}:"
+                    "\n" + traceback.format_exc()
+                )
+    for job_object_id in old_job_object_ids:
+        del job_objects[job_object_id]
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

src/meadowrun/run_job_local_main.py
@@ -87,25 +87,38 @@ import logging
 import os
 import sys
 import traceback
-from typing import Optional, Tuple, TYPE_CHECKING
+from typing import Optional, Tuple, TYPE_CHECKING, Union
 
+import meadowrun.func_worker_storage_helper
 import meadowrun.run_job_local
 from meadowrun.azure_integration.blob_storage import get_azure_blob_container
 from meadowrun.meadowrun_pb2 import ProcessState, Job
 from meadowrun.run_job_core import CloudProvider, CloudProviderType, get_log_path
-from meadowrun.storage_grid_job import get_aws_s3_bucket_async
-import meadowrun.func_worker_storage_helper
+from meadowrun.storage_grid_job import get_aws_s3_bucket
+from meadowrun.storage_keys import job_id_to_storage_key_process_state
 
 if TYPE_CHECKING:
     from meadowrun.deployment_manager import StorageBucketFactoryType
+    from meadowrun.abstract_storage_bucket import AbstractStorageBucket
 
 
 async def main_async(
     job_id: str,
+    job_object_id: Optional[str],
     public_address: Optional[str],
     cloud: Optional[Tuple[CloudProviderType, str]],
 ) -> None:
     job_io_prefix = f"/var/meadowrun/io/{job_id}"
+    if job_object_id:
+        job_object_path = f"/var/meadowrun/io/{job_object_id}.job_to_run"
+    else:
+        job_object_path = f"{job_io_prefix}.job_to_run"
+
+    storage_bucket: Optional[AbstractStorageBucket] = None
+    storage_bucket_factory: Union[
+        StorageBucketFactoryType, AbstractStorageBucket, None
+    ] = None
+
     try:
         # write to a temp file and then rename to make sure deallocate_tasks doesn't see
         # a partial write
@@ -113,24 +126,27 @@ async def main_async(
             f.write(str(os.getpid()))
         os.rename(f"{job_io_prefix}.pid_temp", f"{job_io_prefix}.pid")
 
-        with open(f"{job_io_prefix}.job_to_run", mode="rb") as f:
+        # get the job
+        with open(job_object_path, mode="rb") as f:
             bytes_job_to_run = f.read()
         job = Job()
         job.ParseFromString(bytes_job_to_run)
 
-        if cloud is None:
-            storage_bucket_factory: StorageBucketFactoryType = None
-        elif cloud[0] == "EC2":
-            storage_bucket_factory = functools.partial(
-                get_aws_s3_bucket_async, cloud[1]
-            )
-        elif cloud[0] == "AzureVM":
-            storage_bucket_factory = functools.partial(
-                get_azure_blob_container, cloud[1]
-            )
-        else:
-            print(f"Warning: unknown value for cloud {cloud}")
-            storage_bucket_factory = None
+        # set storage_bucket, storage_bucket_factory
+        if cloud is not None:
+            if cloud[0] == "EC2":
+                # with EC2, we need the storage bucket to download the job object
+                storage_bucket = await get_aws_s3_bucket(cloud[1]).__aenter__()
+                storage_bucket_factory = storage_bucket
+            elif cloud[0] == "AzureVM":
+                # with Azure, the storage bucket is optional, so storage_bucket_factory
+                # is a function rather than an actual storage_bucket
+                storage_bucket_factory = functools.partial(
+                    get_azure_blob_container, cloud[1]
+                )
+            else:
+                print(f"Warning: unknown value for cloud {cloud}")
+                storage_bucket_factory = None
 
         if public_address:
             # this is a little silly as we could just use the IMDS endpoints to figure
@@ -150,17 +166,21 @@ async def main_async(
             storage_bucket_factory,
             compile_environment_in_container=True,
         )
-        with open(f"{job_io_prefix}.initial_process_state", mode="wb") as f:
-            f.write(first_state.SerializeToString())
 
         if (
             first_state.state != ProcessState.ProcessStateEnum.RUNNING
             or continuation is None
         ):
-            with open(f"{job_io_prefix}.process_state", mode="wb") as f:
-                f.write(first_state.SerializeToString())
+            final_process_state = first_state
         else:
             final_process_state = await continuation
+
+        if storage_bucket is not None:
+            await storage_bucket.write_bytes(
+                final_process_state.SerializeToString(),
+                job_id_to_storage_key_process_state(job_id),
+            )
+        else:
             # if the result is large it's a little sad because we're duplicating it into
             # this .process_state file
             with open(f"{job_io_prefix}.process_state", mode="wb") as f:
@@ -172,6 +192,8 @@ async def main_async(
         traceback.print_exc()
         raise
     finally:
+        if storage_bucket is not None:
+            await storage_bucket.__aexit__(None, None, None)
         if cloud is not None:
             # we want to kick this off and then allow the current process to complete
             # without affecting the child process. This seems to work on Linux but not
@@ -190,11 +212,12 @@ async def main_async(
 
 def main(
     job_id: str,
+    job_object_id: Optional[str],
     public_address: Optional[str],
     cloud: Optional[Tuple[CloudProviderType, str]],
 ) -> None:
     try:
-        asyncio.run(main_async(job_id, public_address, cloud))
+        asyncio.run(main_async(job_id, job_object_id, public_address, cloud))
     except (KeyboardInterrupt, asyncio.CancelledError):
         print("Job was killed by SIGINT")
 
@@ -207,6 +230,7 @@ def command_line_main() -> None:
     parser.add_argument("--public-address")
     parser.add_argument("--cloud", choices=CloudProvider)
     parser.add_argument("--cloud-region-name")
+    parser.add_argument("--job-object-id")
     args = parser.parse_args()
 
     if bool(args.cloud is None) ^ bool(args.cloud_region_name is None):
@@ -220,7 +244,7 @@ def command_line_main() -> None:
     else:
         cloud = args.cloud, args.cloud_region_name
 
-    main(args.job_id, args.public_address, cloud)
+    main(args.job_id, args.job_object_id, args.public_address, cloud)
 
 
 if __name__ == "__main__":

commit b0d696483247fd31023049493b7eb25970f98b4e

b896443 broke run_command/run_function on EC2, fixing that. Relatedly, adding a comment explaining why we launch jobs over SSH for run_command/run_function. adc178c also broke the tests that read the log files, fixing that.

src/meadowrun/aws_integration/ec2_instance_allocation.py
@@ -734,7 +734,12 @@ async def run_job_ec2_instance_registrar(
             job.ports,
         )
 
-    # TODO consider switching this to use the SQS route?
+    # For run_map on EC2, we launch jobs via SQS/machine_agent.py, but for
+    # run_function/run_command we just lanch them directly with SSH. At some point we
+    # should probably get rid of this "launch jobs via SSH" code and replace it all with
+    # SSH but it feels weird to do that because for run_job/run_command we're always
+    # going to start the job via SQS/machine_agent.py and then still create an SSH
+    # connection to tail the logs.
     return await SshHost(host, SSH_USER, pkey, ("EC2", region_name)).run_cloud_job(
         job, job_id, wait_for_result, None
     )

src/meadowrun/aws_integration/machine_agent.py
@@ -183,7 +183,7 @@ async def _launch_jobs(
                     "/var/meadowrun/env/bin/python -m meadowrun.run_job_local_main "
                     f"--job-id {job_id} --cloud EC2 --cloud-region-name {region_name} "
                     f"--job-object-id {job_object_id} {public_address} "
-                    f"> {get_log_path(job_id)} 2>&1",
+                    f"--results-to-storage > {get_log_path(job_id)} 2>&1",
                     shell=True,
                     env=environment,
                     # see https://stackoverflow.com/a/4791612/908704

src/meadowrun/run_job_local_main.py
@@ -94,7 +94,7 @@ import meadowrun.run_job_local
 from meadowrun.azure_integration.blob_storage import get_azure_blob_container
 from meadowrun.meadowrun_pb2 import ProcessState, Job
 from meadowrun.run_job_core import CloudProvider, CloudProviderType, get_log_path
-from meadowrun.storage_grid_job import get_aws_s3_bucket
+from meadowrun.storage_grid_job import get_aws_s3_bucket, get_aws_s3_bucket_async
 from meadowrun.storage_keys import job_id_to_storage_key_process_state
 
 if TYPE_CHECKING:
@@ -107,6 +107,7 @@ async def main_async(
     job_object_id: Optional[str],
     public_address: Optional[str],
     cloud: Optional[Tuple[CloudProviderType, str]],
+    results_to_storage: bool,
 ) -> None:
     job_io_prefix = f"/var/meadowrun/io/{job_id}"
     if job_object_id:
@@ -135,12 +136,22 @@ async def main_async(
         # set storage_bucket, storage_bucket_factory
         if cloud is not None:
             if cloud[0] == "EC2":
-                # with EC2, we need the storage bucket to download the job object
-                storage_bucket = await get_aws_s3_bucket(cloud[1]).__aenter__()
-                storage_bucket_factory = storage_bucket
+                if results_to_storage:
+                    # we need the storage bucket to upload our results
+                    storage_bucket = await get_aws_s3_bucket(cloud[1]).__aenter__()
+                    # storage_bucket_factory can be either a real storage bucket or a
+                    # function that returns a storage bucket
+                    storage_bucket_factory = storage_bucket
+                else:
+                    storage_bucket_factory = functools.partial(
+                        get_aws_s3_bucket_async, cloud[1]
+                    )
             elif cloud[0] == "AzureVM":
-                # with Azure, the storage bucket is optional, so storage_bucket_factory
-                # is a function rather than an actual storage_bucket
+                if results_to_storage:
+                    raise NotImplementedError(
+                        "--results-to-storage is not yet implemented for Azure"
+                    )
+
                 storage_bucket_factory = functools.partial(
                     get_azure_blob_container, cloud[1]
                 )
@@ -215,9 +226,12 @@ def main(
     job_object_id: Optional[str],
     public_address: Optional[str],
     cloud: Optional[Tuple[CloudProviderType, str]],
+    results_to_storage: bool,
 ) -> None:
     try:
-        asyncio.run(main_async(job_id, job_object_id, public_address, cloud))
+        asyncio.run(
+            main_async(job_id, job_object_id, public_address, cloud, results_to_storage)
+        )
     except (KeyboardInterrupt, asyncio.CancelledError):
         print("Job was killed by SIGINT")
 
@@ -231,6 +245,7 @@ def command_line_main() -> None:
     parser.add_argument("--cloud", choices=CloudProvider)
     parser.add_argument("--cloud-region-name")
     parser.add_argument("--job-object-id")
+    parser.add_argument("--results-to-storage", action="store_true")
     args = parser.parse_args()
 
     if bool(args.cloud is None) ^ bool(args.cloud_region_name is None):
@@ -244,7 +259,13 @@ def command_line_main() -> None:
     else:
         cloud = args.cloud, args.cloud_region_name
 
-    main(args.job_id, args.job_object_id, args.public_address, cloud)
+    main(
+        args.job_id,
+        args.job_object_id,
+        args.public_address,
+        cloud,
+        args.results_to_storage,
+    )
 
 
 if __name__ == "__main__":

tests/automated/aws/test_basics.py
@@ -39,12 +39,15 @@ class AwsHostProvider(HostProvider):
         return "https://github.com/meadowdata/test_repo"
 
     async def get_log_file_text(self, job_completion: JobCompletion) -> str:
+        log_file_name = job_completion.log_file_name
+        if log_file_name.startswith(job_completion.public_address + ":"):
+            log_file_name = log_file_name[len(job_completion.public_address) + 1 :]
         async with ssh.connect(
             job_completion.public_address,
             username=SSH_USER,
             private_key=get_meadowrun_ssh_key(REGION),
         ) as conn:
-            return await ssh.read_text_from_file(conn, job_completion.log_file_name)
+            return await ssh.read_text_from_file(conn, log_file_name)
 
 
 class TestDeploymentsAws(AwsHostProvider, DeploymentSuite):

tests/automated/test_azure_automated.py
@@ -51,13 +51,16 @@ class AzureHostProvider(HostProvider):
         return "https://github.com/meadowdata/test_repo"
 
     async def get_log_file_text(self, job_completion: JobCompletion) -> str:
+        log_file_name = job_completion.log_file_name
+        if log_file_name.startswith(job_completion.public_address + ":"):
+            log_file_name = log_file_name[len(job_completion.public_address) + 1 :]
         private_key, public_key = await ensure_meadowrun_key_pair("eastus")
         async with ssh.connect(
             job_completion.public_address,
             username="meadowrunuser",
             private_key=private_key,
         ) as conn:
-            return await ssh.read_text_from_file(conn, job_completion.log_file_name)
+            return await ssh.read_text_from_file(conn, log_file_name)
 
     def get_num_concurrent_tasks(self) -> int:
         # default quota on Azure is very low (3vCPUs)